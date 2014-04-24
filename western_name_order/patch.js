6a2845d14e4455b1cee6ffafd80d7806aac688a4
nmlgc <nmlgc@nmlgc.net>
2014-04-24 23:22:15 +0200

    Import western_name_order from Touhou Patch Center.

---
+++ western_name_order/patch.js
@@ -0,0 +1,11 @@
+{
+	"servers": [
+		"http://srv.thpatch.net/western_name_order/",
+		"http://thpatch.nmlgc.net/western_name_order/"
+	],
+	"id": "western_name_order",
+	"title": "Western name order",
+	"dependencies": [
+		"base_tsa"
+	]
+}
