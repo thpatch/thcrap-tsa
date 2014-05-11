1d18ce78ed1ecf662d4296c3b8a8eee8033d8117
nmlgc <nmlgc@nmlgc.net>
2014-05-11 11:50:29 +0200

    Add file breakpoints for th143 v1.00a.

---
+++ base_tsa/th143.js
@@ -0,0 +1,19 @@
+{
+	"title": "弾幕アマノジャク　～ Impossible Spell Card",
+	"latest": "v1.00a",
+	"breakpoints": {
+		"file_size": {
+			"file_size": "eax",
+			"file_name": "ebx",
+			"cavesize": "6"
+		},
+		"file_load": {
+			"file_buffer": "eax",
+			"stack_clear_size": "8",
+			"cavesize": "5"
+		},
+		"file_loaded": {
+			"cavesize": "5"
+		}
+	}
+}
---
+++ base_tsa/th143.v1.00a.js
@@ -0,0 +1,13 @@
+{
+	"breakpoints": {
+		"file_size": {
+			"addr": "Rx2e89"
+		},
+		"file_load": {
+			"addr": "Rx2ec4"
+		},
+		"file_loaded": {
+			"addr": "Rx68d62"
+		}
+	}
+}
