3b4877a1f58a1ab9af48bdfb5546168dcd9cac8f
nmlgc <nmlgc@nmlgc.net>
2014-05-12 14:29:07 +0200

    Add dialog string IDs for th143's custom.exe.

---
+++ base_tsa/th143_custom/dialog_103.js
@@ -0,0 +1,33 @@
+{
+	"title": "th143 config",
+	"font": "Dialog font",
+	"items": [
+		"th07 DirectInput",
+		"th10 slowshot",
+		"th10 screen mode",
+		"th14 Dialog 640",
+		"th14 Dialog 960",
+		"th14 Dialog 1280",
+		"th06 colors",
+		"th07 32bits mode",
+		"th06 16bits mode",
+		"th06 refresh rate",
+		"th09 full rate",
+		"th06 half rate",
+		"th06 third rate",
+		"th11 input latency",
+		"th11 stable input",
+		"th11 normal input",
+		"th11 automatic input",
+		"th11 fast input",
+		"th09 no save",
+		"th09 save",
+		"th11 input description",
+		"th14 Dialog Always ask",
+		null,
+		"th11 sensitivity description",
+		"th11 joypad sensitivity",
+		"th11 default",
+		"th14 Dialog Fullscreen"
+	]
+}
