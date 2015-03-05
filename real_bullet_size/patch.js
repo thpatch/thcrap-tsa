450e030b5be373fdb831d42be1939c59562d8564
nmlgc <nmlgc@nmlgc.net>
2015-03-05 10:31:50 +0100

    Import sibgamer's bullet resizing patch.
    Can we have 19 more people like DTM? Please?
    
    By all means, if you have a patch and can't be bothered to set up a server to
    host it, try contacting anyone in the thcrap patch network. I didn't put those
    contact e-mail addresses there for nothing, y'know. Don't count on me reading
    MoTK because it's supposed to be "the most important site". I only do that if
    I happen to be in the mood for some self-harm.

---
+++ real_bullet_size/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "real_bullet_size",
+	"servers": [
+		"http://thcrap.nmlgc.net/repos/nmlgc/real_bullet_size/",
+		"http://mirrors.thpatch.net/nmlgc/real_bullet_size/"
+	],
+	"title": "Resize bullets to match their actual hitbox\n                          (by sibgamer, http://tinyurl.com/real-bullet-size)"
+}
--- repo.js
+++ repo.js
@@ -8,6 +8,7 @@
 		"aero": "Enable Aero compositing",
 		"base_tsa": "Basic support for Team Shanghai Alice games",
 		"instant_ending": "Show the ending immediately upon entering Stage 1",
+		"real_bullet_size": "Resize bullets to match their actual hitbox\n                          (by sibgamer, http://tinyurl.com/real-bullet-size)",
 		"script_latin": "Shared data for languages using Latin script",
 		"tsa_en": "Official English translation by ZUN",
 		"western_name_order": "Western name order"
