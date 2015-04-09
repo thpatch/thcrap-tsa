31a10a909c8f47d8c2880673c20c760a41233302
nmlgc <nmlgc@nmlgc.net>
2015-04-09 19:57:56 +0200

    Create a separate patch for Marisa's "Bitch, get out of the way" line in TH08.
    As implicitly requested by r/touhou, and probably some other people I can't
    remember right now.

---
+++ bitch/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "bitch",
+	"servers": [
+		"http://thcrap.nmlgc.net/repos/nmlgc/bitch/",
+		"http://mirrors.thpatch.net/nmlgc/bitch/"
+	],
+	"title": "Keep Marisa's \"bitch\" line in TH08"
+}
--- repo.js
+++ repo.js
@@ -7,6 +7,7 @@
 	"patches": {
 		"aero": "Enable Aero compositing",
 		"base_tsa": "Basic support for Team Shanghai Alice games",
+		"bitch": "Keep Marisa's \"bitch\" line in TH08",
 		"instant_ending": "Show the ending immediately upon entering Stage 1",
 		"real_bullet_size": "Resize bullets to match their actual hitbox\n                          (by sibgamer, http://tinyurl.com/real-bullet-size)",
 		"script_latin": "Shared data for languages using Latin script",
