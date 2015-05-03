49439cf41a1cdf31007df7e0099915673dd2251b
nmlgc <nmlgc@nmlgc.net>
2015-05-03 09:16:39 +0200

    instant_ending: Add support for th128.
    Which thankfully has no bad endings, but needs to run for at least two stages
    before the game even knows which ending to show.

---
+++ instant_ending/th128.v1.00a.js
@@ -0,0 +1,10 @@
+{
+	"binhacks": {
+		"instant_ending": {
+			"title": "Show the ending after the first two stages of a route",
+			"addr": "Rx24595",
+			"expected": "83f8 03 0f84 f7000000  83f8 05 0f84 ee000000  83f8 08 0f84 e5000000  83f8 0a 0f84 dc000000  83f8 0d 0f84 d3000000  83f8 0f",
+			"code":     "83f8 02 0f84 f7000000  83f8 04 0f84 ee000000  83f8 07 0f84 e5000000  83f8 09 0f84 dc000000  83f8 0c 0f84 d3000000  83f8 0e"
+		}
+	}
+}
