fb2377283a2811b3be1682ebdf51e15f31b54bfc
Zrrg <hiroaritillwhen@gmail.com>
2015-07-12 22:09:26 +0200

    th09 v1.50a: Locate known hardcoded strings.

---
+++ base_tsa/th09/stringlocs.v1.50a.js
@@ -0,0 +1,21 @@
+{
+	"Rx8fb4c": "th09 Menu Story Start",
+	"Rx8fb2c": "th09 Menu Extra Start",
+	"Rx8fb00": "th09 Menu Match Start",
+	"Rx8fae8": "th09 Menu Replay",
+	"Rx8fad8": "th09 Menu Result",
+	"Rx8fac4": "th09 Menu Music Room",
+	"Rx8fab0": "th09 Option BGM",
+	"Rx8f7dc": "th09 Option Key Config",
+	"Rx8fa50": "th09 Key Side",
+	"Rx8fa1c": "th09 Key Control Type",
+	"Rx8f9e4": "th09 Key Charge Type",
+	"Rx8f9c8": "th09 Key Shot",
+	"Rx8f9b0": "th09 Key Bomb",
+	"Rx8f988": "th09 Key Slow",
+	"Rx8f764": "th09 Music Room spoiler 1",
+	"Rx8f72c": "th09 Music Room spoiler 2",
+	"Rx8f6e8": "th09 Music Room spoiler 3",
+	"Rx8fab0": "th07 Menu Option",
+	"Rx8fa98": "th07 Menu Quit"
+}
