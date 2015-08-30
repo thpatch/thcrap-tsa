thpatch/thcrap-tsa

commit bf1e1922a8b7443868bb85dbe8a047c136a34547

th15 v1.00a: Locate Hardcoded Strings
Locates known hardcoded strings in th15 and th15_custom as well as
updated the stringdefs.js in script_latin to accommodate the names of
the new game.

base_tsa/files.js
@@ -116,6 +116,8 @@
 	"th15.v0.01b.js": 1608510306,
 	"th15.v1.00a.js": 1923592262,
 	"th15/ascii/ascii_960.png": 2497078814,
+	"th15/stringlocs.v1.00a.js": 1966540250,
 	"th15/text.v1.00a.anm": 2869000509,
+	"th15_custom/stringlocs.v1.00a": 1550664420,
 	"versions.js": 294189761
 }

base_tsa/th15/stringlocs.v1.00a.js
@@ -1,0 +1,23 @@
+{
+	"Rxcdef0": "th10 Result Known Spell",
+	"Rxcdf08": "th10 Result Unknown Spell",
+	"Rxce168": "Music Room Unknown Title",
+	"Rxcd08c": "th10_ascii_stage_1",
+	"Rxcd094": "th10_ascii_stage_2",
+	"Rxcd09c": "th10_ascii_stage_3",
+	"Rxcd0a4": "th10_ascii_stage_4",
+	"Rxcd0ac": "th10_ascii_stage_5",
+	"Rxcd0b4": "th10_ascii_stage_6",
+	"Rxcd0bc": "th10_ascii_stage_clear",
+	"Rxcd1c0": "th10_ascii_ranking",
+	"Rxcdf80": "th10_ascii_score_empty",
+	"Rxcd21c": "th13_ascii_credit",
+	"Rxcd53c": "th15 JP",
+	"Rxcd8dc": "th14_ascii_bonus",
+	"Rxcd8e8": "th14_ascii_no_bonus",
+	"Rxce044": "th10 Music Room spoiler 1",
+	"Rxce080": "th10 Music Room spoiler 2",
+	"Rxce0b8": "th10 Music Room spoiler 3",
+	"Rxce0e4": "th10 Music Room spoiler 4",
+	"Rxce120": "th10 Music Room spoiler 5"
+}

base_tsa/th15_custom/stringlocs.v1.00a.js
@@ -1,0 +1,4 @@
+{
+	"Rxc1d4": "th06 config saved caption",
+	"Rxc1dc": "th06 config saved text"
+}

script_latin/files.js
@@ -5,7 +5,7 @@
 	"alcostg.js": null,
 	"global.js": 3370491521,
 	"patch.js": 378718977,
-	"stringdefs.js": 3986092678,
+	"stringdefs.js": 286395310,
 	"th01.js": null,
 	"th02.js": null,
 	"th03.js": null,

script_latin/stringdefs.js
@@ -18,6 +18,7 @@
 	"th13": "Touhou Shinreibyou - Ten Desires",
 	"th14": "Touhou Kishinjou - Double Dealing Character",
 	"th143": "Danmaku Amanojaku - Impossible Spell Card",
+	"th15": "Touhou Kanjuden - Legacy of Lunatic Kingdom",
 	"th10 JP": "Touhou Fuujinroku",
 	"alcostg JP": "Tasogare Sakaba",
 	"th11 JP": "Touhou Chireiden",
@@ -27,6 +28,7 @@
 	"th13 JP": "Touhou Shinreibyou",
 	"th14 JP": "Touhou Kishinjou",
 	"th143 JP": "Danmaku Amanojaku",
+	"th15 JP": "Touhou Kanjuden",
 	"th13 Spell Practice Unknown": " No.%3d  ???????????",
 	"th10 Result Unknown Spell": "No. <r$%3d$999>  ????????????????????? <r$%4d/%4d$>",
 	"Music Room Unknown Title": "No. <r$%2d$99>  ???????????",

commit 4aaeb0dea4f20872596470bbc36e3525fe266f91

th15: Fix bug introduced in bf1e1922a8b7443868bb85dbe8a047c136a34547

base_tsa/files.js
@@ -118,7 +118,7 @@
 	"th15/ascii/ascii_960.png": 2497078814,
 	"th15/dialog_203.js": 3906570971,
 	"th15/dialog_204.js": 3906570971,
-	"th15/stringlocs.v1.00a.js": 2236664331,
+	"th15/stringlocs.v1.00a.js": 377643180,
 	"th15/text.v1.00a.anm": 2869000509,
 	"th15_custom/dialog_3.js": 2438523396,
 	"th15_custom/dialog_103.bin": 3814449392,

base_tsa/th15/stringlocs.v1.00a.js
@@ -1,6 +1,4 @@
 {
-	"Rxcdef0": "th10 Result Known Spell",
-	"Rxcdf08": "th10 Result Unknown Spell",
 	"Rxce168": "Music Room Unknown Title",
 	"Rxcd08c": "th10_ascii_stage_1",
 	"Rxcd094": "th10_ascii_stage_2",