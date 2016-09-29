164c4bd8c78592599fcd1469b2264ca2f7eb002b
nmlgc <nmlgc@nmlgc.net>
2016-09-30 01:18:18 +0200

    th12 v1.00b: Locate known hardcoded strings.

---
+++ base_tsa/th12/stringlocs.v1.00b.js
@@ -0,0 +1,13 @@
+{
+	"Rxa0ddc": "th10_ascii_stage_clear",
+	"Rxa0de4": "th10_ascii_stage_6",
+	"Rxa0dec": "th10_ascii_stage_5",
+	"Rxa0df4": "th10_ascii_stage_4",
+	"Rxa0dfc": "th10_ascii_stage_3",
+	"Rxa0fc4": "th10_ascii_ranking",
+	"Rxa1cb8": "th10 Music Room spoiler 5",
+	"Rxa1cf0": "th10 Music Room spoiler 4",
+	"Rxa1d2c": "th10 Music Room spoiler 3",
+	"Rxa1d58": "th10 Music Room spoiler 2",
+	"Rxa1d90": "th10 Music Room spoiler 1"
+}
