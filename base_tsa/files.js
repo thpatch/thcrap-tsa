a3ca85576584c0e196026b335ba1c147b06c1db3
Egor <DankRank@users.noreply.github.com>
2016-11-19 01:41:27 +0200

    th08: Added hardcoded strings for results menu
    Some of those strings appear twice in the executable. In those
    cases the bottommost stringloc is the one that is actually used
    in the menu.
    
    I will post stringdefs for ja and en later, along with pull request.

--- base_tsa/files.js
+++ base_tsa/files.js
@@ -31,7 +31,7 @@
 	"th07_custom/stringlocs.v1.00b.js": 1542694573,
 	"th08.js": 2377303692,
 	"th08.v1.00d.js": 1583682437,
-	"th08/stringlocs.v1.00d.js": 1841507372,
+	"th08/stringlocs.v1.00d.js": 1290961298,
 	"th08_custom/dialog_103.js": 908462822,
 	"th08_custom/stringlocs.v1.00.js": 1542694573,
 	"th09.js": 2119314694,
