a293701fad1fb9fb52d0bd28c86404260c806f92
Egor <DankRank@users.noreply.github.com>
2016-11-29 19:04:37 +0200

    th08: Added spell unlock conditions.

--- base_tsa/files.js
+++ base_tsa/files.js
@@ -31,7 +31,7 @@
 	"th07_custom/stringlocs.v1.00b.js": 1542694573,
 	"th08.js": 4075974922,
 	"th08.v1.00d.js": 3963054847,
-	"th08/stringlocs.v1.00d.js": 3531389721,
+	"th08/stringlocs.v1.00d.js": 2908214505,
 	"th08_custom/dialog_103.js": 908462822,
 	"th08_custom/stringlocs.v1.00.js": 1542694573,
 	"th09.js": 2119314694,
