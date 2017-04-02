e599512a8f074603ca598d1bec76e7ad27407d8b
Bruno Liron <bruno.liron@epitech.eu>
2017-04-02 11:55:57 +0200

    Move th145 from the sandbox to base_tasofro

--- base_tasofro/files.js
+++ base_tasofro/files.js
@@ -3,5 +3,6 @@
 	"patch.js": 3345798038,
 	"th145.js": 3958036584,
 	"th145.v1.31b.js": 372333060,
-	"th145/fileslist.txt": 3963881002
+	"th145/fileslist.txt": 3963881002,
+	"versions.js": 783350646
 }
---
+++ base_tasofro/versions.js
@@ -0,0 +1,16 @@
+{
+    "hashes": {
+        "f8229b5908f3dee003803593eb14e7ae927560b746adae6ece82b3b7c0d6e8a9": [
+            "th145",
+            "v1.31b",
+            "(original)"
+        ]
+    },
+    "sizes": {
+        "4831744": [
+            "th145",
+            "v1.31b",
+            "(original)"
+        ]
+    }
+}
