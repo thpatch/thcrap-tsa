303839195ad2d88f917391dbaa98421075d1c23b
nmlgc <nmlgc@nmlgc.net>
2017-05-07 17:05:32 +0200

    th16: Translate the resolution dialog.

--- base_tsa/files.js
+++ base_tsa/files.js
@@ -139,6 +139,8 @@
 	"th16.js": 3947710424,
 	"th16.v0.01a.js": 3482866176,
 	"th16/ascii/ascii_960.png": 2497078814,
+	"th16/dialog_203.js": 2545567094,
+	"th16/dialog_204.js": 2545567094,
 	"th16/stringlocs.v0.01a.js": 1987749458,
 	"th16/text.v0.01a.anm": 3824339000,
 	"versions.js": 2568148991
