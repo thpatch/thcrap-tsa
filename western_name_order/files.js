8c9f887e830007fb3bd7034681046359aa52bee5
nmlgc <nmlgc@nmlgc.net>
2017-05-10 10:19:06 +0200

    western_name_order: Add ename.png for TH16.
    Staying neutral on the Etarnity/Eternity issue, since the opinions seem
    about evenly split when looking at all translations on Touhou Patch
    Center.
    Interestingly though, Aunn/Aun is not nearly as contested.
    
    (Also had to shift the image down by one pixel, to ensure that the
    shadow stays on the correct sprite.)

--- western_name_order/files.js
+++ western_name_order/files.js
@@ -6,5 +6,6 @@
 	"th13/front/ename_full.png": 604414306,
 	"th14/front/ename.png": 1289038687,
 	"th14/front/ename_tr.png": 1401179187,
-	"th15/front/ename.png": 2268154010
+	"th15/front/ename.png": 2268154010,
+	"th16/front/ename.png": 611478432
 }
