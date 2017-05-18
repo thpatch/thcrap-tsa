ecfade47458d76549b20a62ff65a8014a2dc5dfa
brliron <brliron@hotmail.fr>
2017-05-18 22:26:37 +0200

    thcrap_tasofro: Add balloon filenames to fileslist.txt

--- base_tasofro/files.js
+++ base_tasofro/files.js
@@ -6,6 +6,6 @@
 	"th145/fileslist.txt": 3963881002,
 	"th155.js": 2529519672,
 	"th155.v0.01a.js": 3115951521,
-	"th155/fileslist.txt": 133095947,
+	"th155/fileslist.txt": 718707390,
 	"versions.js": 1776541720
 }
