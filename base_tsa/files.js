8b599bc2c3f5514260ad6811b5aba0be4f5a7533
brliron <brliron@hotmail.fr>
2017-06-18 23:40:09 +0200

    Update version number

--- base_tsa/files.js
+++ base_tsa/files.js
@@ -9,7 +9,7 @@
 	"alcostg_custom/dialog_103.js": 450950490,
 	"alcostg_custom/stringlocs.v1.00a.js": 724832697,
 	"formats.js": 290473403,
-	"global.js": 2816678249,
+	"global.js": 3878759097,
 	"patch.js": 1114190375,
 	"stringdefs.js": 1722675708,
 	"th06.js": 3353611440,
