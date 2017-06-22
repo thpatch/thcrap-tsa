cc7a455607c916fe63740f59364b467921a5b532
brliron <brliron@hotmail.fr>
2017-06-22 23:41:07 +0200

    Update version number

--- base_tasofro/global.js
+++ base_tasofro/global.js
@@ -1,3 +1,3 @@
 {
-	"thcrap_version_min": "0x20170618"
+	"thcrap_version_min": "0x20170622"
 }
--- base_tsa/global.js
+++ base_tsa/global.js
@@ -1,5 +1,5 @@
 {
-	"thcrap_version_min": "0x20170618",
+	"thcrap_version_min": "0x20170622",
 	"thcrap_url": "http://thpatch.net/wiki/Project:Download",
 	"binhacks": {
 		"log_restore": {
