108024990d7c8230a47f2961527cc976b385bbed
Bruno Liron <b.liron@ateme.com>
2017-06-22 10:03:18 +0200

    Tasofro: add namelist_right to the known filenames.

--- base_tasofro/files.js
+++ base_tasofro/files.js
@@ -3,9 +3,9 @@
 	"patch.js": 3345798038,
 	"th145.js": 620829315,
 	"th145.v1.31b.js": 1845079523,
-	"th145/fileslist.txt": 3963881002,
+	"th145/fileslist.txt": 3036226125,
 	"th155.js": 2529519672,
 	"th155.v0.01a.js": 3115951521,
-	"th155/fileslist.txt": 718707390,
+	"th155/fileslist.txt": 1622631139,
 	"versions.js": 1776541720
 }
