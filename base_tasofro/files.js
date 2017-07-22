a828cf303ac992151cfc070a2bad797f1a969c8f
brliron <brliron@hotmail.fr>
2017-07-22 14:38:27 +0200

    Add a few new files to fileslist.txt

--- base_tasofro/files.js
+++ base_tasofro/files.js
@@ -6,7 +6,7 @@
 	"th145.v1.41.js": 54143643,
 	"th145/data/plugin/se_libact.dll.jdiff": 2946775874,
 	"th145/data/plugin/se_libact.v1.41.dll.jdiff": 643171614,
-	"th145/fileslist.txt": 271905720,
+	"th145/fileslist.txt": 187219195,
 	"th155.js": 2529519672,
 	"th155.v0.01a.js": 3115951521,
 	"th155/fileslist.txt": 1622631139,
