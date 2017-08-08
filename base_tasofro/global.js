4bf1a397cacb09ba11f82a60fe078274dda5a115
brliron <brliron@hotmail.fr>
2017-08-08 20:10:52 +0200

    Update version number

--- base_tasofro/global.js
+++ base_tasofro/global.js
@@ -1,3 +1,3 @@
 {
-	"thcrap_version_min": "0x20170715"
+	"thcrap_version_min": "0x20170808"
 }
--- base_tsa/files.js
+++ base_tsa/files.js
@@ -9,7 +9,7 @@
 	"alcostg_custom/dialog_103.js": 450950490,
 	"alcostg_custom/stringlocs.v1.00a.js": 724832697,
 	"formats.js": 290473403,
-	"global.js": 3634363440,
+	"global.js": 2475467801,
 	"patch.js": 1114190375,
 	"stringdefs.js": 1722675708,
 	"th06.js": 3353611440,
