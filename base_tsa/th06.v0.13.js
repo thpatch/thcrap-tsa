d1356a507127958ac8fd1fa4032a327b10d96c5c
nmlgc <nmlgc@nmlgc.net>
2017-08-19 22:36:33 +0200

    th06 v0.13: Correctly scale rotated sprites in 3D space from textures wider than 256 pixels.
    I should have just waited until we have fast breakpoints and patched it
    that way instead of going through *this*. Oh well, not too hard, just
    different.
    
    sprite3d_rotated_voodookill
    ---------------------------
    • After the REP MOVSD above, EDI points directly after the local copy of
      the world matrix. Since we need element [0][0] next, roll EDI back to
      the beginning of the D3DXMATRIX structure by subtracting its size.
      • Savings by shortened offsets: 3 bytes.
      • Savings by reusing the register for the call to
        D3DXMatrixMultiply(): 8 bytes.
    • Before the call to D3DXMatrixRotationX():
      • Directly push the X rotation value at [EBP], without loading it into
        EAX first. Savings: 1 byte.
      • Load address of the rotation matrix into ESI and keep it there.
        • Savings by reusing the register for the call to
          D3DXMatrixMultiply(): 4 bytes.
        • Since it comes immediately after EDI, we can load it relative to
          that and save 1 more byte.
    • Mutiply the X scaling value with (texture_width / 256.0f):
    
      MOV   ECX, [EBP+sprite_instance_t.spec] (6)
      FLD   [EAX+sprite_spec_t.texture_width] (3)
      FDIV  [256.0f]                          (6) ; version-specific…
      FMULP ST(1)                             (2)
    
      Cost: 17 bytes.
    • Replace FCOMP [pointer to a 0.0f constant] with FLDZ+FCOMPP. Savings:
      2 bytes, 1 memory reference.
    
    • NOP out the 2 bytes we saved.

--- base_tsa/th06.v0.13.js
+++ base_tsa/th06.v0.13.js
@@ -14,6 +14,10 @@
 		},
 		"text_sprite_width": {
 			"addr": ["Rx226f7", "Rx22786", "Rx227d4", "Rx228a7", "Rx22933", "Rx2298a"]
+		},
+		"sprite3d_rotated_voodookill": {
+			"addr": "Rx21544",
+			"code": "83ef40 d907 d84d1c 8b8dc0000000 d94118 d83528554500 dec9 d91f d94714 d84d18 d9e0 d95f14 d94500 d9ee ded9 dfe0 f6c444 7b16 ff7500 8d7740 56 e8078d0000 56 57 57 9090"
 		}
 	},
 	"breakpoints": {
