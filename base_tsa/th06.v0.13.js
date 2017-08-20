e9b1bab5cdebbcc4135fb55dd2084e29ba226314
nmlgc <nmlgc@nmlgc.net>
2017-08-20 18:40:24 +0200

    th06 v0.13: Correctly scale unrotated sprites in 3D space from textures wider than 256 pixels.
    Well, turns out that in order to implement this as a breakpoint, we
    *really* want to have address expression evaluation. Oh, and our N is
    still small enough that making breakpoint calls O(1) isn't actually
    worth it right now. So, a 4th binary hack it is. At least we're done
    with this now.
    
    sprite3d_unrotated_voodookill
    -----------------------------
    • Roll back the world matrix pointer in EDI to the first element. This
      has to happen before the TEST, which resets the flags.
      • Cost of rollback: 3 bytes.
      • Savings by shortened offsets: 9 bytes.
      • Savings by reusing the register for the call to
        IDirect3DDevice8::SetTransform(): 4 bytes.
    • Since we'll have some space left: Replace FDIV [pointer to a 2.0f
      constant] with FLD1+FLD1+FADDP+FDIVP. Cost: 2 bytes, savings: 1 memory
      reference.
    • Move the entire SetTransform() call to the end for better readability.
    • Save EAX to the stack so that the sprite instance pointer doesn't have
      to be reloaded from [ESP+94h] after the call to SetTransform().
      Savings: 5 bytes.
    • Mutiply the X scaling value with (texture_width / 256.0f):
    
      FLD   [EAX+sprite_spec_t.texture_width] (3)
      FDIV  [256.0f]                          (6) ; The hack is version-
      FMULP ST(1)                             (2) ; specific-anyway.
    
      Cost: 11 bytes.
    
    • NOP out the 2 bytes we saved.

--- base_tsa/th06.v0.13.js
+++ base_tsa/th06.v0.13.js
@@ -18,6 +18,10 @@
 		"sprite3d_rotated_voodookill": {
 			"addr": "Rx21544",
 			"code": "83ef40 d907 d84d1c 8b8dc0000000 d94118 d83528554500 dec9 d91f d94714 d84d18 d9e0 d95f14 d94500 d9ee ded9 dfe0 f6c444 7b16 ff7500 8d7740 56 e8078d0000 56 57 57 9090"
+		},
+		"sprite3d_unrotated_voodookill": {
+			"addr": "Rx218ca",
+			"code": "83ef40 8b90c0000000 f6c502 7414 d9422c d84818 d9e8 d9e8 dec1 def9 d86f34 d95f34 8b8898000000 894f38 d907 d94218 d83528554500 dec9 d8481c d91f d94714 d84818 d9e0 d95f14 50 57 6800010000 a178236b00 50 8b10 ff9294000000 5a 9090"
 		}
 	},
 	"breakpoints": {
