73261b3c0ee5146464060fe5c7965b61b9ac970e
brliron <brliron@hotmail.fr>
2017-08-24 19:41:11 +0200

    Oops, I forgot the files.js

--- base_tasofro/files.js
+++ base_tasofro/files.js
@@ -1,14 +1,17 @@
 {
 	"global.js": 3739079435,
 	"patch.js": 3345798038,
-	"th145.js": 3323782504,
+	"th135.js": 3736808575,
+	"th135.v1.34b.js": 3345237793,
+	"th135/fileslist.txt": 3317738791,
+	"th145.js": 1248278040,
 	"th145.v1.31b.js": 1845079523,
 	"th145.v1.41.js": 54143643,
 	"th145/data/plugin/se_libact.dll.jdiff": 2946775874,
 	"th145/data/plugin/se_libact.v1.41.dll.jdiff": 643171614,
 	"th145/fileslist.txt": 187219195,
-	"th155.js": 2529519672,
+	"th155.js": 570416742,
 	"th155.v0.01a.js": 3115951521,
 	"th155/fileslist.txt": 1622631139,
-	"versions.js": 3472451023
+	"versions.js": 896501986
 }
