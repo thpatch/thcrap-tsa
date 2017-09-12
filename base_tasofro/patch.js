2d322508b496076bf6dc4dead87957a991f567be
brliron <brliron@hotmail.fr>
2017-09-13 00:21:43 +0200

    Update version number

--- base_tasofro/patch.js
+++ base_tasofro/patch.js
@@ -1,5 +1,6 @@
 {
 	"id": "base_tasofro",
+	"thcrap_version_min": "0x20170913",
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/nmlgc/base_tasofro/",
 		"http://mirrors.thpatch.net/nmlgc/base_tasofro/"
