819b92f89352a2328c51853737893f8759b58bfb
nmlgc <nmlgc@nmlgc.net>
2017-09-13 23:43:40 +0200

    Sort `thcrap_version_min` alphabetically, matching repo_update.py's output.
    Sure, we could disable that `sort_keys` flag in scripts/utils.py. But
    ultimately, repo_update.py will always change patch.js since it
    propagates the `"servers"` array from repo.js. So we do have to keep the
    overall format of repo.js and patch.js identical to the one written by
    repo_update.py.
    
    At least the cronjob sends me a mail whenever a merge failed due to a
    local change by repo_update.py that's not reflected in the GitHub repo.
    Still better than silent files.js failures. :p
    
    And yeah, getting rid of the `"servers"` in patch.js and having
    thcrap_update fall back on repo.js and construct the proper part would
    have been better, but doing it now will break automatic updates for all
    those old builds still out there. Which are a lot.

--- base_tasofro/patch.js
+++ base_tasofro/patch.js
@@ -1,9 +1,9 @@
 {
 	"id": "base_tasofro",
-	"thcrap_version_min": "0x20170914",
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/nmlgc/base_tasofro/",
 		"http://mirrors.thpatch.net/nmlgc/base_tasofro/"
 	],
+	"thcrap_version_min": "0x20170914",
 	"title": "Basic support for Tasogare Frontier games"
 }
