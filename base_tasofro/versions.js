993248d24835f3a17e1e64fcad111111258f17d4
nmlgc <nmlgc@nmlgc.net>
2017-10-19 22:22:17 +0200

    Reformat versions.js for better readability in an editor.
    The original one was really just the way the Touhou Patch Center
    extension formats all JSON files.

--- base_tasofro/versions.js
+++ base_tasofro/versions.js
@@ -1,51 +1,19 @@
 {
     "hashes": {
-        "111948df4129e27b18b96cedeafbec7e536eef1aa2fe23547b86793f93456ac0": [
-            "th135",
-            "v1.34b",
-            "(original)"
-        ],
-        "f8229b5908f3dee003803593eb14e7ae927560b746adae6ece82b3b7c0d6e8a9": [
-            "th145",
-            "v1.31b",
-            "(original)"
-        ],
-        "6c0b4e2fda1d1991a60a733d47abbdf541a8fc8cdf0c0737fc7e1dd89d846307": [
-            "th145",
-            "v1.41",
-            "(original)"
-        ],
-        "d940b32a5165e4b0a0b3743746530d0a72cb19c3b127f244e802732a05063f4d": [
-            "th155",
-            "v0.01a",
-            "(original)"
-        ],
-        "ba8a68c68ab160c016f0683d993eff0e1b18eb145f84358c90fdd59fc2d48435": [
-            "th155",
-            "v0.01a",
-            "(no verify trust)"
-        ]
+        "111948df4129e27b18b96cedeafbec7e536eef1aa2fe23547b86793f93456ac0": ["th135", "v1.34b", "(original)"],
+
+        "f8229b5908f3dee003803593eb14e7ae927560b746adae6ece82b3b7c0d6e8a9": ["th145", "v1.31b", "(original)"],
+        "6c0b4e2fda1d1991a60a733d47abbdf541a8fc8cdf0c0737fc7e1dd89d846307": ["th145", "v1.41",  "(original)"],
+
+        "d940b32a5165e4b0a0b3743746530d0a72cb19c3b127f244e802732a05063f4d": ["th155", "v0.01a", "(original)"],
+        "ba8a68c68ab160c016f0683d993eff0e1b18eb145f84358c90fdd59fc2d48435": ["th155", "v0.01a", "(no verify trust)"]
     },
     "sizes": {
-        "5454336": [
-            "th135",
-            "v1.34b",
-            "(original)"
-        ],
-        "4831744": [
-            "th145",
-            "v1.31b",
-            "(original)"
-        ],
-        "4876288": [
-            "th145",
-            "v1.41",
-            "(original)"
-        ],
-        "5123760": [
-            "th155",
-            "v0.01a",
-            "(original)"
-        ]
+        "5454336": ["th135", "v1.34b", "(original)"],
+
+        "4831744": ["th145", "v1.31b", "(original)"],
+        "4876288": ["th145", "v1.41",  "(original)"],
+
+        "5123760": ["th155", "v0.01a", "(original)"]
     }
 }
