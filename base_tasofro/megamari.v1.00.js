49b107c459b329c21dda8346b3d082e47c2dba88
brliron <brliron@hotmail.fr>
2017-11-05 00:24:22 +0100

    base_tasofro: add megamari support

---
+++ base_tasofro/megamari.v1.00.js
@@ -0,0 +1,13 @@
+{
+	"breakpoints": {
+		"nsml_file_header": {
+			"addr": "Rx74E79"
+		},
+		"nsml_read_file#prepare": {
+			"addr": "Rx688A4"
+		},
+		"nsml_read_file": {
+			"addr": "Rx750CD"
+		}
+	}
+}
--- base_tasofro/versions.js
+++ base_tasofro/versions.js
@@ -1,27 +1,31 @@
 {
     "hashes": {
-        "111948df4129e27b18b96cedeafbec7e536eef1aa2fe23547b86793f93456ac0": ["th135", "v1.34b", "(original)"],
+        "111948df4129e27b18b96cedeafbec7e536eef1aa2fe23547b86793f93456ac0": ["th135",    "v1.34b", "(original)"],
 
-        "f8229b5908f3dee003803593eb14e7ae927560b746adae6ece82b3b7c0d6e8a9": ["th145", "v1.31b", "(original)"],
-        "6c0b4e2fda1d1991a60a733d47abbdf541a8fc8cdf0c0737fc7e1dd89d846307": ["th145", "v1.41",  "(original)"],
+        "f8229b5908f3dee003803593eb14e7ae927560b746adae6ece82b3b7c0d6e8a9": ["th145",    "v1.31b", "(original)"],
+        "6c0b4e2fda1d1991a60a733d47abbdf541a8fc8cdf0c0737fc7e1dd89d846307": ["th145",    "v1.41",  "(original)"],
 
-        "d940b32a5165e4b0a0b3743746530d0a72cb19c3b127f244e802732a05063f4d": ["th155", "v0.01a", "(original)"],
-        "ba8a68c68ab160c016f0683d993eff0e1b18eb145f84358c90fdd59fc2d48435": ["th155", "v0.01a", "(no verify trust)"],
-        "c50f0e280aacf2e77445646f26aed0940ddbd603146a9b52c4fba62b466d7ec9": ["th155", "v0.021", "(original)"],
+        "d940b32a5165e4b0a0b3743746530d0a72cb19c3b127f244e802732a05063f4d": ["th155",    "v0.01a", "(original)"],
+        "ba8a68c68ab160c016f0683d993eff0e1b18eb145f84358c90fdd59fc2d48435": ["th155",    "v0.01a", "(no verify trust)"],
+        "c50f0e280aacf2e77445646f26aed0940ddbd603146a9b52c4fba62b466d7ec9": ["th155",    "v0.021", "(original)"],
 
-        "185b3ee84de981749bafe0fb7bf9b6f61c33617ef60a86c1648d5e4e9bc2301c": ["nsml",  "v1.00",  "(original)"],
-        "2db975a408e260499d52126f25ecf2fbc529cad52d1bffc2a0b7ca2ff695f155": ["nsml",  "v1.10",  "(original)"]
+        "185b3ee84de981749bafe0fb7bf9b6f61c33617ef60a86c1648d5e4e9bc2301c": ["nsml",     "v1.00",  "(original)"],
+        "2db975a408e260499d52126f25ecf2fbc529cad52d1bffc2a0b7ca2ff695f155": ["nsml",     "v1.10",  "(original)"],
+
+        "4a4749cf36c7b98f7ad9a4e73a88d13b319ffdf2b61e16230a36398eb2908380": ["megamari", "v1.00",  "(original)"]
     },
     "sizes": {
-        "5454336": ["th135", "v1.34b", "(original)"],
+        "5454336": ["th135",    "v1.34b", "(original)"],
 
-        "4831744": ["th145", "v1.31b", "(original)"],
-        "4876288": ["th145", "v1.41",  "(original)"],
+        "4831744": ["th145",    "v1.31b", "(original)"],
+        "4876288": ["th145",    "v1.41",  "(original)"],
 
-        "5123760": ["th155", "v0.01a", "(original)"],
-        "5238960": ["th155", "v0.021", "(original)"],
+        "5123760": ["th155",    "v0.01a", "(original)"],
+        "5238960": ["th155",    "v0.021", "(original)"],
 
-        "1191424": ["nsml",  "v1.00",  "(original)"],
-        "1193984": ["nsml",  "v1.10",  "(original)"]
+        "1191424": ["nsml",     "v1.00",  "(original)"],
+        "1193984": ["nsml",     "v1.10",  "(original)"],
+
+        "860160":  ["megamari", "v1.00",  "(original)"]
     }
 }
