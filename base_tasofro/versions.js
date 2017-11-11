92bd7d60c43b5581862c5ccd103f02eb92d05af0
brliron <brliron@hotmail.fr>
2017-11-11 16:41:55 +0100

    th145: add more known versions (should cover any version between v1.00 and v1.41)

--- base_tasofro/versions.js
+++ base_tasofro/versions.js
@@ -2,6 +2,10 @@
     "hashes": {
         "111948df4129e27b18b96cedeafbec7e536eef1aa2fe23547b86793f93456ac0": ["th135",    "v1.34b", "(original)"],
 
+        "407a0475387ab07f40de3a33c9e50a027dcd7858be0782b347a694c6b858c2d9": ["th145",    "v1.00",  "(original)"],
+        "e514a7cd6119d43173f35ee89a4d52be6eafb246b70aad14d302126705434cc3": ["th145",    "v1.10",  "(original)"],
+        "ec2c21d2701501cb3db8b90bf084b5c17849bf09aefa65ac7c815a8537575ca1": ["th145",    "v1.22b", "(original)"],
+        "c645db83ac93b7f065b0a73d9e9abcf4f5d4901cde445d40ac806fbf792acd86": ["th145",    "v1.30",  "(original)"],
         "f8229b5908f3dee003803593eb14e7ae927560b746adae6ece82b3b7c0d6e8a9": ["th145",    "v1.31b", "(original)"],
         "6c0b4e2fda1d1991a60a733d47abbdf541a8fc8cdf0c0737fc7e1dd89d846307": ["th145",    "v1.41",  "(original)"],
 
@@ -17,6 +21,10 @@
     "sizes": {
         "5454336": ["th135",    "v1.34b", "(original)"],
 
+        "5983744": ["th145",    "v1.00",  "(original)"],
+        "5996544": ["th145",    "v1.10",  "(original)"],
+        "5997056": ["th145",    "v1.22b", "(original)"],
+        "4829696": ["th145",    "v1.30",  "(original)"],
         "4831744": ["th145",    "v1.31b", "(original)"],
         "4876288": ["th145",    "v1.41",  "(original)"],
 
