6866017a44881680daefb05f0ef650b7db062d8f
brliron <brliron@hotmail.fr>
2017-11-14 00:46:03 +0100

    script_latin: th145: remove antialiasing for some font sizes.

---
+++ script_latin/th145.js
@@ -0,0 +1,10 @@
+{
+	"fontrules": {
+		"* 19 * *": "* * * * NONANTIALIASED_QUALITY",
+		"* 20 * *": "* * * * NONANTIALIASED_QUALITY",
+		"* 21 * *": "* * * * NONANTIALIASED_QUALITY",
+		"* 22 * *": "* * * * NONANTIALIASED_QUALITY",
+		"* 36 * *": "* * * * NONANTIALIASED_QUALITY",
+		"* 62 * *": "* * * * NONANTIALIASED_QUALITY"
+	}
+}
