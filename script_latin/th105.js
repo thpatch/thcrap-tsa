d87a85ca3706460d95da3168d0bde465c7994cf9
brliron <brliron@hotmail.fr>
2017-12-13 11:14:42 +0100

    base_tasofro: change the font size, offset and spacing in the endings.

--- script_latin/th105.js
+++ script_latin/th105.js
@@ -5,7 +5,24 @@
 			"title": "Change the font spacing in story mode, making place for a 3rd line."
 		}
 	},
+	"breakpoints": {
+		"th105_font_spacing": {
+			"font_size":    "[esi+0x114]",
+			"y_offset":     "[esi+0x128]",
+			"font_spacing": "[esi+0x130]",
+			"new_y_offset": {
+				"24": 1,
+				"16": 0
+			},
+			"new_spacing": {
+				"24": 1,
+				"16": 2
+			},
+			"cavesize": 7
+		}
+	},
 	"fontrules": {
-		"* 24": "* 22"
+		"* 24": "* 22",
+		"* 16": "* 18"
 	}
 }
--- script_latin/th105.v1.06a.js
+++ script_latin/th105.v1.06a.js
@@ -3,5 +3,10 @@
 		"story_mode_font_spacing": {
 			"addr": "Rx05a242"
 		}
+	},
+	"breakpoints": {
+		"th105_font_spacing": {
+			"addr": "Rx0119A0"
+		}
 	}
 }
