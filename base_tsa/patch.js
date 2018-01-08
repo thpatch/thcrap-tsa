35163e3608043f6167b31ffdcc95de3055ae9fa9
brliron <brliron@hotmail.fr>
2018-01-08 22:31:58 +0100

    Update version number

--- base_tsa/patch.js
+++ base_tsa/patch.js
@@ -5,7 +5,7 @@
 		"http://thcrap.nmlgc.net/repos/nmlgc/base_tsa/",
 		"http://mirrors.thpatch.net/nmlgc/base_tsa/"
 	],
-	"thcrap_version_min": "0x20180106",
+	"thcrap_version_min": "0x20180108",
 	"title": "Basic support for Team Shanghai Alice games",
 	"url_engine": "http://thpatch.net/wiki/Project:Download\n\nThis is the last build you'll ever have to download and extract manually, because we now have digitally signed automatic updates of thcrap itself!"
 }
