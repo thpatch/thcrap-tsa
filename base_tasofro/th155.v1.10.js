9f0224371e7ae85f1f3f67bb99558a20f5113b47
brliron <brliron@hotmail.fr>
2018-04-27 00:34:54 +0200

    base_tasofro: add th155 v1.10 binahcks.
    Not adding versions.js yet because v1.10 support isn't finished yet.

--- base_tasofro/th155.v1.10.js
+++ base_tasofro/th155.v1.10.js
@@ -10,21 +10,21 @@
 	},
 	"breakpoints": {
 		"th135_file_header": {
-			"addr": "Rx025e68"
+			"addr": "Rx025ef8"
 		},
 		"th135_file_name": {
-			"addr": "Rx032cee"
+			"addr": "Rx032d8e"
 		},
 		"th135_read_file#prepare": {
 			"addr": [
-				"Rx02df0c",
-				"Rx02df54"
+				"Rx02df9c",
+				"Rx02dfe4"
 			]
 		},
 		"th135_read_file": {
 			"addr": [
-				"Rx02df11",
-				"Rx02df5d"
+				"Rx02dfa1",
+				"Rx02dfed"
 			]
 		},
 		"detour_plugin": {
