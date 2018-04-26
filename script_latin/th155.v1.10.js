00e5babb230f449b8b92cd20f966803ea79686f0
brliron <brliron@hotmail.fr>
2018-04-27 01:35:40 +0200

    script_latin: th155: add v1.10

---
+++ script_latin/th155.v1.10.js
@@ -0,0 +1,19 @@
+{
+	"binhacks": {
+		"remove_bmpfont_limit": {
+			"code": "909090909090",
+			"addr": "Rx05d4ac"
+		},
+		"remove_sjis_test": {
+			"addr": "Rx065c56"
+		},
+		"push_char": {
+			"addr": "Rx065d15"
+		}
+	},
+	"breakpoints": {
+		"bmpfont_fix_parameters": {
+			"addr": "Rx065d10"
+		}
+	}
+}
