85be31469d3c6fa88c086cf6264b56774dfa6019
nmlgc <nmlgc@nmlgc.net>
2018-08-10 05:37:11 +0200

    th165 v1.00a: Add file patching breakpoints.

--- base_tsa/th165.js
+++ base_tsa/th165.js
@@ -1,3 +1,18 @@
 {
-	"title": "秘封ナイトメアダイアリー　〜 Violet Detector"
+	"title": "秘封ナイトメアダイアリー　〜 Violet Detector",
+	"breakpoints": {
+		"file_size": {
+			"file_size": "eax",
+			"file_name": "ebx",
+			"cavesize": 5
+		},
+		"file_load": {
+			"file_buffer": "eax",
+			"stack_clear_size": 8,
+			"cavesize": 5
+		},
+		"file_loaded": {
+			"cavesize": 5
+		}
+	}
 }
---
+++ base_tsa/th165.v1.00a.js
@@ -0,0 +1,13 @@
+{
+	"breakpoints": {
+		"file_size": {
+			"addr": "Rx0204c"
+		},
+		"file_load": {
+			"addr": "Rx02084"
+		},
+		"file_loaded": {
+			"addr": "Rx5ccd8"
+		}
+	}
+}
