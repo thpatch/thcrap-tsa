ef8a77b05faa470059ae1f8fe3e1b5646cd9f74a
nmlgc <nmlgc@nmlgc.net>
2018-08-10 05:54:18 +0200

    th165 v1.00a: Declare the font block.

--- base_tsa/th165.v1.00a.js
+++ base_tsa/th165.v1.00a.js
@@ -9,5 +9,16 @@
 		"file_loaded": {
 			"addr": "Rx5ccd8"
 		}
-	}
+	},
+	"tsa_font_block": [
+		"Rx50f8e4",
+		"Rx50f904",
+		"Rx50f8fc",
+		"Rx50f90c",
+		"Rx50f8e8",
+		"Rx50f908",
+		"Rx50f900",
+		"Rx50f910",
+		"Rx50f8e0"
+	]
 }
