ed9247ba3b567bb434c29e1d21a460d5fd1680a4
nmlgc <nmlgc@nmlgc.net>
2018-08-10 10:33:39 +0200

    th165 v1.00a: Add safe sprintf for right-aligned text.

--- base_tsa/th165.v1.00a.js
+++ base_tsa/th165.v1.00a.js
@@ -1,10 +1,13 @@
 {
 	"binhacks": {
 		"sprintf_msvcrt14_esp+34": {
-			"addr": ["Rx73d59"]
+			"addr": ["Rx73d59", "Rx73e79"]
 		},
 		"sprintf_rep": {
-			"addr": ["Rx73df6", "Rx73e07"]
+			"addr": [
+				"Rx73df6", "Rx73e07",
+				"Rx73ec4", "Rx73f67", "Rx73f78"
+			]
 		},
 		"music_title_prepare": {
 			"addr": "Rx5ae1f",
