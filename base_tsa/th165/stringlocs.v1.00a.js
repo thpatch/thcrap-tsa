efc62c01a58b637936d4f4c3778d4d0cc3528080
nmlgc <nmlgc@nmlgc.net>
2018-08-11 17:06:03 +0200

    th165 v1.00a: Locate known hardcoded strings.

---
+++ base_tsa/th165/stringlocs.v1.00a.js
@@ -0,0 +1,12 @@
+{
+	"Rx9d060": "th165 JP",
+	"Rx9d9e0": "th10 Music Room spoiler 1",
+	"Rx9da20": "th10 Music Room spoiler 2",
+	"Rx9da58": "th10 Music Room spoiler 3",
+	"Rx9da84": "th10 Music Room spoiler 4",
+	"Rx9dac0": "th10 Music Room spoiler 5",
+	"Rx9db08": "Music Room Unknown Title",
+	"Rx9dc2c": "th143 Trophy Get",
+	"Rx9dc40": "th143 Trophy Format",
+	"Rx9d9b8": "th143 Trophy Unknown"
+}
