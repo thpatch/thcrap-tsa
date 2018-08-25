6d16ff3b6bbf09a1a3d9151df967c556612421f9
nmlgc <nmlgc@nmlgc.net>
2018-08-25 06:42:10 +0200

    Officially support th06.
    Yes, after almost 6 years, all text limits have now been removed, and I
    now officially allow you to translate this game, without the Sandbox
    patch. 🎉
    
    I'll leave out that
    1a24d75df3aad73c3211a1e342ce8ca60bf8dd304da17bbe6a24994b53e4a500 that
    Bruno identified one day (see
    https://www.thpatch.net/w/index.php?title=Patch:Sandbox&diff=prev&oldid=572819),
    because I'd like to know what this one actually is.
    
    Funded by DTM.

--- base_tsa/versions.js
+++ base_tsa/versions.js
@@ -1,5 +1,14 @@
 {
 	"hashes": {
+		"21cddb2b5a7dfaf38e542b707873a35b3fbfd38c1aa21d5b969e1a49a1855f4b": ["th06", "v0.13",  "(original)"],
+		"7152c0ce2667ff10bd5bbe9c3a2052302dec7d326798eb2e70f106e933831a9d": ["th06", "v1.00",  "(original)"],
+		"0adcf7ad5b451d77b5fb86771c3718d242fc7b27dc5940477d289a797e51785f": ["th06", "v1.02f", "(original)"],
+		"9f76483c46256804792399296619c1274363c31cd8f1775fafb55106fb852245": ["th06", "v1.02h", "(original)"],
+		"7f38496b31b8625196900a69cd1bfed243cab1f9612073e7881dc354b876fd39": ["th06", "v1.02h", "Original with 紅魔郷 removed from the .dat file names"],
+		"0883c6433b77af87054d9d05731649c79926a55037840c72d33e50635d64d506": ["th06", "v1.02h", "English patch"],
+		"fa6562ddfd81f3010d7d87792a69aaa950e0f60b00e42885ef2c30577d8dbe45": ["th06", "v1.02h", "English patch with a leftover .cfg removed"],
+		"8a509709650a83db6850c3498e1e1051dddc6cd46791911c0d78438be8968195": ["th06", "v1.02h", "Russian patch", 1251],
+
 		"a44d2581d5a51c5ac93ec2331045e7b29e7386a2414f700097dfab7cd789a34c": ["th07", "v0.01",   "(original)"],
 		"4e0d82301a3dcad4d8cde64aeaf0fdad85bb12941fc4381b744ed8c1be1cdedc": ["th07", "v0.02",   "(original)"],
 		"6c07a9f90ff2a60fe2c1d4f04573aae440baa040c7d87a335d9aa4ff6483b4de": ["th07", "v0.03",   "(original)"],
@@ -157,6 +166,10 @@
 		"abca7c23404b1ee4cecc153acf225d21b3d6d403653a61852545a772fb18e5c6": ["th165_custom", "v1.00a", "(original)"]
 	},
 	"sizes": {
+		"430080": ["th06", "v0.13",  "(original)"],
+		"512000": ["th06", "v1.02h", "(original)"],
+		"684032": ["th06", "v1.02h", "Russian patch", 1251],
+
 		"620000": ["th07", "v0.04b", "(original)"],
 		"628000": ["th07", "v0.06",  "(original)"],
 		"596992": ["th07", "v0.11",  "(original)"],
