9e33d8cda2129d2e01cecb915c84e8b3c7fd4c17
brliron <brliron@hotmail.fr>
2018-09-10 19:09:34 +0200

    Update version number

--- base_tasofro/patch.js
+++ base_tasofro/patch.js
@@ -4,6 +4,6 @@
 		"http://thcrap.nmlgc.net/repos/nmlgc/base_tasofro/",
 		"http://mirrors.thpatch.net/nmlgc/base_tasofro/"
 	],
-	"thcrap_version_min": "0x20180904",
+	"thcrap_version_min": "0x20180910",
 	"title": "Basic support for Tasogare Frontier games"
 }
