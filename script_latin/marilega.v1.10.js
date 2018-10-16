3dc8179a906690793edc85e15036a23db43dcc63
brliron <brliron@hotmail.fr>
2018-10-17 00:31:43 +0200

    script_latin: marilega: add bitmap font support

---
+++ script_latin/marilega.v1.10.js
@@ -0,0 +1,19 @@
+{
+	"binhacks": {
+		"remove_bmpfont_limit": {
+			"code": "909090909090",
+			"addr": "Rx050ffc"
+		},
+		"remove_sjis_test": {
+			"addr": "Rx059836"
+		},
+		"push_char": {
+			"addr": "Rx0598f5"
+		}
+	},
+	"breakpoints": {
+		"bmpfont_fix_parameters": {
+			"addr": "Rx0598f0"
+		}
+	}
+}
