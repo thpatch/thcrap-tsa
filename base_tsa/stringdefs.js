6bf0bccb5741f7985e5af47392580249df83cb42
nmlgc <nmlgc@nmlgc.net>
2019-02-19 17:47:25 +0100

    th06: Enforce UTF-8 for the Japanese in-game BGM format and BGM titles.
    Which 1) allows patches that don't want to use images to translate
    either one and not end up with mojibake, and 2) still doesn't cause
    mojibake when running just base_tsa without a translation, as it now
    contains both parts of the combined string.
    
    The drawback of this rather dumb workaround is, of course, that we undo
    BGM translations made by underlying static patches. What we *actually*
    would have wanted there would be automatic conversion from the fallback
    encoding to UTF-8 in thcrap's strings_sprintf().
    Oh, and Japanese→target language dictionaries… *but*:
    • I wasn't funded for that much, and really need to stop blowing these
      pushes out of proportion. Heck, 42d870e already took way longer than
      the cheap bonus it was supposed to be.
    • I've looked at the code for a while, and concluded that I'd basically
      have to do both the conversion *and* the dictionary lookup at the same
      time if I don't want to rewrite it all later.
    • Especially since strings_sprintf() is pretty hot code and the result
      should at least run decently fast.
    • Oh, and the static Russian patch exists, reminding me once again that
      stringlocs weren't the terrible idea they often seem to be.
    
    Funded by zorg.

--- base_tsa/stringdefs.js
+++ base_tsa/stringdefs.js
@@ -1,4 +1,5 @@
 {
+	"th06 BGM In-game format": "♪%s",
 	"th10 Result Known Spell": "No. <r$%3d$999>  %s <r$%4d/%4d$>",
 	"th10 Result Unknown Spell": "No. <r$%3d$999>  ？？？？？？？？？？？？？？？？？？？？？ <r$%4d/%4d$>",
 	"th15 Result Known Spell": "No.<r$%s%s%s$９９９> %s <r$%4d/%4d$>",
--- base_tsa/th06/stringlocs.v1.02h.js
+++ base_tsa/th06/stringlocs.v1.02h.js
@@ -1,4 +1,5 @@
 {
+	"Rx6a98c": "th06 BGM In-game format",
 	"Rx6a3f4": "th06 Bomb Reimu A",
 	"Rx6a410": "th06 Bomb Reimu B",
 	"Rx6a420": "th06 Bomb Marisa A",
