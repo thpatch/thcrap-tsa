8d63b7a56b2dfe5fe2b63210b344d65ca1373f11
nmlgc <nmlgc@nmlgc.net>
2019-02-23 22:23:54 +0100

    th06: Provide correctly-aligned defaults for certain hardcoded ASCII strings.
    • `centered_stage_demo`: Originally has a single space in front.
    • `replay_format`: Left-align (rather than right-align) player and
      rank strings.
    • `replay_stage_*`, `replay_save_*`: Re-align for 10-digit scores

--- base_tsa/stringdefs.js
+++ base_tsa/stringdefs.js
@@ -1,5 +1,13 @@
 {
 	"th06 BGM In-game format": "♪%s",
+	"th06_ascii_centered_stage_demo": "DEMO PLAY",
+	"th06_ascii_replay": "%s %8s  %8s %-7s  %-7s",
+	"th06_ascii_replay_save_header": "No.   Name     Date     Player  Score",
+	"th06_ascii_replay_save": "No.%.2d %8s %8s %7s %10d",
+	"th06_ascii_replay_save_empty": "No.%.2d -------- --/--/-- -------          0",
+	"th06_ascii_replay_stage_header": "Stage   LastScore",
+	"th06_ascii_replay_stage": "%-6s %10d",
+	"th06_ascii_replay_stage_empty": "%-6s  ---------",
 	"th10 Result Known Spell": "No. <r$%3d$999>  %s <r$%4d/%4d$>",
 	"th10 Result Unknown Spell": "No. <r$%3d$999>  ？？？？？？？？？？？？？？？？？？？？？ <r$%4d/%4d$>",
 	"th15 Result Known Spell": "No.<r$%s%s%s$９９９> %s <r$%4d/%4d$>",
