50fedbc2aa8a017597868fdcc1c2a22590e4bf98
Egor <egor@opensrc.club>
2019-05-05 07:08:09 +0300

    th17tr: Added formats
    didn't check, but these probably haven't changed

--- base_tsa/th17.js
+++ base_tsa/th17.js
@@ -1,3 +1,8 @@
 {
-	"title": "東方鬼形獣 　～ Wily Beast and Weakest Creature"
+	"title": "東方鬼形獣 　～ Wily Beast and Weakest Creature",
+	"formats": {
+		"anm": "anm11",
+		"msg": "msg11",
+		"end": "end10"
+	}
 }
