da8dafc41d0d86120447f80017a4ef9bc57c1cbf
Egor <egor@opensrc.club>
2019-05-07 13:42:39 +0300

    th17tr: recognize hardcoded strings
    this is not the complete set, but this is how much we bothered to do for
    th16

---
+++ base_tsa/th17/stringlocs.v0.01a.js
@@ -0,0 +1,21 @@
+{
+	"Rx9e964": "th15 Full-width 0",
+	"Rx9e968": "th15 Full-width 1",
+	"Rx9e96c": "th15 Full-width 2",
+	"Rx9e970": "th15 Full-width 3",
+	"Rx9e974": "th15 Full-width 4",
+	"Rx9e978": "th15 Full-width 5",
+	"Rx9e97c": "th15 Full-width 6",
+	"Rx9e980": "th15 Full-width 7",
+	"Rx9e984": "th15 Full-width 8",
+	"Rx9e988": "th15 Full-width 9",
+	"Rx9e990": "th15 Result Known Spell",
+	"Rx9e9a8": "th15 Result Unknown Spell",
+	"Rx9eb98": "Music Room Unknown Title",
+	"Rx9ea74": "th10 Music Room spoiler 1",
+	"Rx9eab0": "th10 Music Room spoiler 2",
+	"Rx9eae8": "th10 Music Room spoiler 3",
+	"Rx9eb14": "th10 Music Room spoiler 4",
+	"Rx9eb50": "th10 Music Room spoiler 5",
+	"Rx9ea24": "th10_ascii_score_empty"
+}
