0e2f620305c63cdf91b478d38700bb7779853136
Egor <egor@opensrc.club>
2019-05-07 04:23:44 +0300

    th17tr: resolution dialogue and window title

---
+++ base_tsa/th17/dialog_204.js
@@ -0,0 +1,12 @@
+{
+	"title": "th17 JP",
+	"items": [
+		"th14 Dialog Prompt",
+		"th14 Dialog 640",
+		"th14 Dialog 960",
+		"th14 Dialog 1280",
+		"th14 Dialog Fullscreen",
+		"th14 Dialog Always ask",
+		"th11 Dialog Game Start"
+	]
+}
