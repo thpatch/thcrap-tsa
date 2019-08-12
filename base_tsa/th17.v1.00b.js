a505a45e38b79e8d508da02321f244832309a09e
Egor <egor@opensrc.club>
2019-08-12 08:42:40 +0300

    th17.v1.00b.js: reorder the file

--- base_tsa/th17.v1.00b.js
+++ base_tsa/th17.v1.00b.js
@@ -1,4 +1,40 @@
 {
+	"binhacks": {
+		"sprintf_msvcrt14_esp+34": {
+			"addr": [
+				"Rx75039",
+				"Rx75159"
+			]
+		},
+		"sprintf_msvcrt14_esp+3c": {
+			"addr": "Rx752c9"
+		},
+		"sprintf_msvcrt14_ebp+10c": {
+			"addr": "Rx082cf"
+		},
+		"sprintf_rep": {
+			"addr": [
+				"Rx082dd",
+				"Rx750d6",
+				"Rx750e7",
+				"Rx751a4",
+				"Rx75247",
+				"Rx75258",
+				"Rx7539d"
+			]
+		},
+		"spell_align": {
+			"code": "ff7518 56 e8[GetTextExtentForFontID] 83c008 d1e0 89c6 8b442410 8b4c240c 8b542414 8b4c8808 8bc1 c1f808 8b948200018601 0fb6c1 8d0c40 8b8224010000 8b0cc8 8b451c 8d1400 89f0 8b742410 909090",
+			"addr": "Rx745b0"
+		},
+		"spell_align_result": {
+			"addr": "Rx75381"
+		},
+		"music_title_prepare": {
+			"code": "8b542418 52 31c0 50 50 50 a1 209a50000",
+			"addr": "Rx5a71f"
+		}
+	},
 	"breakpoints": {
 		"file_size": {
 			"addr": "Rx0215c"
@@ -43,41 +79,5 @@
 	},
 	"tsa_font_block": {
 		"addr": "Rx12a334"
-	},
-	"binhacks": {
-		"sprintf_msvcrt14_esp+34": {
-			"addr": [
-				"Rx75039",
-				"Rx75159"
-			]
-		},
-		"sprintf_msvcrt14_esp+3c": {
-			"addr": "Rx752c9"
-		},
-		"sprintf_msvcrt14_ebp+10c": {
-			"addr": "Rx082cf"
-		},
-		"sprintf_rep": {
-			"addr": [
-				"Rx082dd",
-				"Rx750d6",
-				"Rx750e7",
-				"Rx751a4",
-				"Rx75247",
-				"Rx75258",
-				"Rx7539d"
-			]
-		},
-		"spell_align": {
-			"code": "ff7518 56 e8[GetTextExtentForFontID] 83c008 d1e0 89c6 8b442410 8b4c240c 8b542414 8b4c8808 8bc1 c1f808 8b948200018601 0fb6c1 8d0c40 8b8224010000 8b0cc8 8b451c 8d1400 89f0 8b742410 909090",
-			"addr": "Rx745b0"
-		},
-		"spell_align_result": {
-			"addr": "Rx75381"
-		},
-		"music_title_prepare": {
-			"code": "8b542418 52 31c0 50 50 50 a1 209a50000",
-			"addr": "Rx5a71f"
-		}
 	}
 }
