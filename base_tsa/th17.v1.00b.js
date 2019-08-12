dd9288598456bdd20024bc00ed3a6f90a8a2266a
32th System <intensonat@gmail.com>
2019-08-12 05:48:42 +0200

    th17: file breakpoints

--- base_tsa/th17.v1.00b.js
+++ base_tsa/th17.v1.00b.js
@@ -1,4 +1,18 @@
 {
+	{
+	"breakpoints": {
+		"file_size": {
+			"addr": "Rx0215c"
+		},
+		"file_load": {
+			"addr": "Rx02194"
+		},
+		"file_loaded": {
+			"addr": "Rx5e238"
+		}
+	}
+}
+
 	"tsa_font_block": {
 		"addr": "Rx12a334"
 	}
