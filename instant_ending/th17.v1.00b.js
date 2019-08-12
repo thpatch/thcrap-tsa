56bcb67f76ed1083e71f5ef1138f2f667f037384
32th System <intensonat@gmail.com>
2019-08-12 09:05:06 +0200

    th17: instant ending

---
+++ instant_ending/th17.v1.00b.js
@@ -0,0 +1,14 @@
+{
+	"binhacks": {
+		"instant_ending": {
+			"addr": "Rx322b5"
+		},
+		"zero_lifes": {
+			"addr": "Rx3044e"
+		},
+		"deaths_to_continues": {
+			"addr": "Rx4921b",
+			"code": "ff05045a4b00"
+		}
+	}
+}
