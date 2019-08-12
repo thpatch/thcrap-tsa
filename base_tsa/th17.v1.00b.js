64a17d2f978a53bc56e1b038b98cb07d95d29723
Egor <egor@opensrc.club>
2019-08-12 07:15:52 +0300

    th17 v1.00b Step 8: Fix sprintf() buffer overflows

--- base_tsa/th17.v1.00b.js
+++ base_tsa/th17.v1.00b.js
@@ -1,5 +1,4 @@
 {
-	{
 	"breakpoints": {
 		"file_size": {
 			"addr": "Rx0215c"
@@ -10,10 +9,33 @@
 		"file_loaded": {
 			"addr": "Rx5e238"
 		}
-	}
-}
-
+	},
 	"tsa_font_block": {
 		"addr": "Rx12a334"
+	},
+	"binhacks": {
+		"sprintf_msvcrt14_esp+34": {
+			"addr": [
+				"Rx75039",
+				"Rx75159"
+			]
+		},
+		"sprintf_msvcrt14_esp+3c": {
+			"addr": "Rx752c9"
+		},
+		"sprintf_msvcrt14_ebp+10c": {
+			"addr": "Rx082cf"
+		},
+		"sprintf_rep": {
+			"addr": [
+				"Rx082dd",
+				"Rx750d6",
+				"Rx750e7",
+				"Rx751a4",
+				"Rx75247",
+				"Rx75258",
+				"Rx7539d"
+			]
+		}
 	}
 }
