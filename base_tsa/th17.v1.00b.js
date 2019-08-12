16532913216341d38152c344e601857721327330
Egor <egor@opensrc.club>
2019-08-12 08:54:14 +0300

    th17.v1.00b: spell_align proper address

--- base_tsa/th17.v1.00b.js
+++ base_tsa/th17.v1.00b.js
@@ -25,7 +25,7 @@
 		},
 		"spell_align": {
 			"code": "ff7518 56 e8[GetTextExtentForFontID] 83c008 d1e0 89c6 8b442410 8b4c240c 8b542414 8b4c8808 8bc1 c1f808 8b948200018601 0fb6c1 8d0c40 8b8224010000 8b0cc8 8b451c 8d1400 89f0 8b742410 909090",
-			"addr": "Rx745b0"
+			"addr": "Rx751b0"
 		},
 		"spell_align_result": {
 			"addr": "Rx75381"
