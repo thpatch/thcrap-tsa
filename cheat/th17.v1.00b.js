f320ae7d599a6d456b5104f4e56f4beb9dd7d0e7
eatsweetly <40319363+eatsweetly@users.noreply.github.com>
2019-09-04 21:30:03 +0900

    Add files via upload

---
+++ cheat/th17.v1.00b.js
@@ -0,0 +1,36 @@
+{
+	"binhacks": {
+		"Main/Infinite lives": {
+			"addr": "0x44921A",
+			"code": "90",
+			"expected": "49"
+		},
+		"Main/Infinite bombs": {
+			"addr": "0x411C96",
+			"code": "909090",
+			"expected": "83C0FF"
+		},
+		"Main/Invincible": {
+			"addr": "0x449420",
+			"code": "C3",
+			"expected": "55"
+		},
+		"Main/Auto-bomb": {
+			"addr": "0x447C19",
+			"code": "C6",
+			"expected": "F6"
+		},
+		"Main/Boss timer freeze#1": {
+			"addr": "0x41A8CF",
+			"code": "EB",
+			"expected": "7C",
+			"ignore": true
+		},
+		"Main/Boss timer freeze#2": {
+			"addr": "0x420A1F",
+			"code": "524E",
+			"expected": "ED4D",
+			"ignore": true
+		}
+	}
+}
