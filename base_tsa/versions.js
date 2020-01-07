8ef99d20dc14147a1ed045d5e68c92b0cae91ee3
Tudi20 <tudorkae@gmail.com>
2020-01-07 23:17:54 +0100

    Change English to English (not recommended)

--- base_tsa/versions.js
+++ base_tsa/versions.js
@@ -5,8 +5,8 @@
 		"0adcf7ad5b451d77b5fb86771c3718d242fc7b27dc5940477d289a797e51785f": ["th06", "v1.02f", "(original)"],
 		"9f76483c46256804792399296619c1274363c31cd8f1775fafb55106fb852245": ["th06", "v1.02h", "(original)"],
 		"7f38496b31b8625196900a69cd1bfed243cab1f9612073e7881dc354b876fd39": ["th06", "v1.02h", "Original with 紅魔郷 removed from the .dat file names"],
-		"0883c6433b77af87054d9d05731649c79926a55037840c72d33e50635d64d506": ["th06", "v1.02h", "English patch"],
-		"fa6562ddfd81f3010d7d87792a69aaa950e0f60b00e42885ef2c30577d8dbe45": ["th06", "v1.02h", "English patch with a leftover .cfg removed"],
+		"0883c6433b77af87054d9d05731649c79926a55037840c72d33e50635d64d506": ["th06", "v1.02h", "English patch (not recommended)"],
+		"fa6562ddfd81f3010d7d87792a69aaa950e0f60b00e42885ef2c30577d8dbe45": ["th06", "v1.02h", "English patch with a leftover .cfg removed (not recommended)"],
 		"8a509709650a83db6850c3498e1e1051dddc6cd46791911c0d78438be8968195": ["th06", "v1.02h", "Russian patch", 1251],
 
 		"a44d2581d5a51c5ac93ec2331045e7b29e7386a2414f700097dfab7cd789a34c": ["th07", "v0.01",   "(original)"],
@@ -20,7 +20,7 @@
 		"1251458d0564c565610b28bc94a434f5d4e8aee5d0882fa278b19277c7ac4cf7": ["th07", "v1.00",   "(original)"],
 		"8198b75faf42ad4ab7e509a089bfbb3a4ffb03927350b70926227a6e26255914": ["th07", "v1.00a",  "(original)"],
 		"35467eaf8dc7fc85f024f16fb2037255f151cefda33cf4867bc9122aaa2e80ca": ["th07", "v1.00b",  "(original)"],
-		"275cfdcab6ad463be4d3d646ddc21811dc65cb1c7e88c7b18bb8a89f25316655": ["th07", "v1.00b",  "English patch"],
+		"275cfdcab6ad463be4d3d646ddc21811dc65cb1c7e88c7b18bb8a89f25316655": ["th07", "v1.00b",  "English patch (not recommended)"],
 
 		"a0e04caa9e51f305009aa434ce5bd787424224b972ad72470a77827d36ac8081": ["th08", "v0.03",  "(original)"],
 		"e287dbb4d19781f2da9c161ac0010df7e52e8c8a238c339838a631cfc3b02682": ["th08", "v1.00",  "(original)"],
@@ -28,29 +28,29 @@
 		"0b94bf89ed9d0011305fba5256af6ec9219b0c9da97735022fd1e5ee0f2aff17": ["th08", "v1.00b", "(original, first version sold to doujin shops)"],
 		"f1a7c5941358b83fa11a9f3619ba3b2e2ef4674a4dca1cfcc423ca21d9889acf": ["th08", "v1.00c", "(original)"],
 		"330fbdbf58a710829d65277b4f312cfbb38d5448b3df523e79350b879213d924": ["th08", "v1.00d", "(original)"],
-		"467085c3c85ceed4b6f5be410e14cd56f1af0b5afa4e0c752270b56ecbc470cf": ["th08", "v1.00d", "English patch"],
+		"467085c3c85ceed4b6f5be410e14cd56f1af0b5afa4e0c752270b56ecbc470cf": ["th08", "v1.00d", "English patch (not recommended)"],
 
 		"de2076269ed3747cdbec840dd3eb7b155407f5e536b8caa83d26fc716e9a67cc": ["th09", "v0.02a", "(original)"],
 		"234195e017548c2114c664a6f5592be64720dad5d445a24602e8fa2e792f97ef": ["th09", "v1.00",  "(original)"],
 		"10350095bcf95edb59e03bee9849a2dc8a7714b4927ad5909c569c550fce6822": ["th09", "v1.50a", "(original)"],
-		"f402287326bcee15b6eda743f1edb7882294257d65c8d828d849110cb0c53bb6": ["th09", "v1.50a", "English patch"],
+		"f402287326bcee15b6eda743f1edb7882294257d65c8d828d849110cb0c53bb6": ["th09", "v1.50a", "English patch (not recommended)"],
 
 		"a4cc00511b45b1f8bb525d0f393e77c4e1bb27cc50f0dc3413d5a8da2e8abb72": ["th095", "v1.00a", "(original)"],
 		"bb54f6fc54f0eeffaec416ca9f64aef32b5f59b7427fa5a6579f6538e0eddc07": ["th095", "v1.02a", "(original)"],
-		"350efaf4585e461e8071d9818f48a34d09bb0782932630155eddba68e65d5089": ["th095", "v1.02a", "English patch v1.03"],
-		"4f80eb7b7037a69dc87b6e39a02db855a24ddd6db6befffb60dc6175e07afc4a": ["th095", "v1.02a", "English patch v1.1"],
+		"350efaf4585e461e8071d9818f48a34d09bb0782932630155eddba68e65d5089": ["th095", "v1.02a", "English patch v1.03 (not recommended)"],
+		"4f80eb7b7037a69dc87b6e39a02db855a24ddd6db6befffb60dc6175e07afc4a": ["th095", "v1.02a", "English patch v1.1 (not recommended)"],
 
 		"5430fac601d645798ccf794362ec7d4cd753814f09d160247dc628b7c6943fd8": ["th10", "v0.02a", "(original)"],
 		"2f14760b6fbbf57549541583283badb9a19a4222b90f0a146d5aa17f01dc9040": ["th10", "v1.00a", "(original)"],
-		"d28e36a6596a4c64b441c148d65a8c12b68e4a683f1d14e7df83d43f92c57d71": ["th10", "v1.00a", "English patch"],
+		"d28e36a6596a4c64b441c148d65a8c12b68e4a683f1d14e7df83d43f92c57d71": ["th10", "v1.00a", "English patch (not recommended)"],
 
 		"ea10d27bb7af17c72f7c4ba21d98ef2d23f36a5625affc40d63de88d963dffd1": ["alcostg", "v1.00a", "(original)"],
 
 		"360f542349a08707de5c788ef5a950d5d3ad0fd727a23e74b6813db40dfe77a9": ["th11", "v0.02a", "(original)"],
 		"2978b17f6184d100d249d4311348dd30c5c32ec75c014b667a525b797d3d8813": ["th11", "v1.00a", "(original)"],
 		"9ab285272251cae99984c9ba15320542ad20a642a0e98a4dd7827d64021ee4a0": ["th11", "v1.00a", "Drake's Boss Rush patch"],
-		"18555e5055909570dbf46ca2a7cb796c50174fcdffb863a83357110d7f3f770b": ["th11", "v1.00a", "English patch"],
-		"e333e600c27e3154f03001605ec3993456e20814212aa183d7ecbc4176a18539": ["th11", "v1.00a", "English patch, Drake's Boss Rush patch"],
+		"18555e5055909570dbf46ca2a7cb796c50174fcdffb863a83357110d7f3f770b": ["th11", "v1.00a", "English patch (not recommended)"],
+		"e333e600c27e3154f03001605ec3993456e20814212aa183d7ecbc4176a18539": ["th11", "v1.00a", "English patch, Drake's Boss Rush patch (not recommended)"],
 		"f42c5d3540489e8c99ac81d88293ef7a830ad3a402d67dcd83f575816775dbd7": ["th11", "v1.00a", "Korean patch (Gungsuh font)"],
 		"5cc754c4ddd52ee788910a479372330645969fffaade5b3a7a42c8b099e8dbbe": ["th11", "v1.00a", "Korean patch (Nanum Gothic font)"],
 		"df6edcf5649b1df917ef91000e3047d49ba3be33301d8d6789b74b1f8b78b435": ["th11", "v1.00a", "Riri's practice patch, v1.03"],
@@ -59,17 +59,17 @@
 		"e2939ba3f92e427216a619191a682a8197cc59a64eab99302a7b98811d6120a9": ["th12", "v1.00a", "(original)"],
 		"d8d644d2e64957a3031b1a1399d0502e1ddaa5252d2c4e492770ad6717827628": ["th12", "v1.00b", "(original)"],
 		"0c9ac1c756eaef681f7538af72b297877ac13f06c2ca2df743e8a29d96672d08": ["th12", "v1.00b", "Score counter bugfix"],
-		"9bcc0675792a057d97a5f560c3d0cf21d07cb02f01d47a9207dcbe1c5efffee5": ["th12", "v1.00b", "English patch"],
-		"d116fed3eef093607f8c4b55fd0ec45dfc99139a7e1913f88570db0968341ba2": ["th12", "v1.00b", "English patch, score counter bugfix"],
-		"7b3d64c41cb078b5cbf5ccce11b14278690e90689f1a1c14dce15b0880efb06c": ["th12", "v1.00b", "English patch, autobomb patch"],
-		"b851680ff45c15ae39257ee7590fcf3c8aba76c839289d94d5a5c30daadb00a1": ["th12", "v1.00b", "English patch, autobomb patch, score counter bugfix"],
+		"9bcc0675792a057d97a5f560c3d0cf21d07cb02f01d47a9207dcbe1c5efffee5": ["th12", "v1.00b", "English patch (not recommended)"],
+		"d116fed3eef093607f8c4b55fd0ec45dfc99139a7e1913f88570db0968341ba2": ["th12", "v1.00b", "English patch, score counter bugfix (not recommended)"],
+		"7b3d64c41cb078b5cbf5ccce11b14278690e90689f1a1c14dce15b0880efb06c": ["th12", "v1.00b", "English patch, autobomb patch (not recommended)"],
+		"b851680ff45c15ae39257ee7590fcf3c8aba76c839289d94d5a5c30daadb00a1": ["th12", "v1.00b", "English patch, autobomb patch, score counter bugfix (not recommended)"],
 
 		"4895ce367f6ab90c6593a94ca0ecfa2aabe7477f0a9b7dd2ee8860327069436c": ["th125", "v1.00a", "(original)"],
-		"a620057a756dd98efbdb813b1bc1350bc5acdf8d462fcd4be93f85eb6c22d9ee": ["th125", "v1.00a", "English patch"],
+		"a620057a756dd98efbdb813b1bc1350bc5acdf8d462fcd4be93f85eb6c22d9ee": ["th125", "v1.00a", "English patch (not recommended)"],
 		"2e0f3ff7434605b49da65602b169b7592ffed4b29f14e10a4464c6a96fa54591": ["th125", "v1.00a", "(original, Steam release)"],
 
 		"ba515750ece845b56640fe440592fc39ebb7bdd1ed87c541b67a19b148e767d4": ["th128", "v1.00a", "(original)"],
-		"f26586b24673d0e1477ebbb698c82470803730e11f950caa428c7d25d6e0a415": ["th128", "v1.00a", "English patch"],
+		"f26586b24673d0e1477ebbb698c82470803730e11f950caa428c7d25d6e0a415": ["th128", "v1.00a", "English patch (not recommended)"],
 		"73781a26417b823c3b1efe7065bb0c93337e9d50634347a1347c5359457f3ae0": ["th128", "v1.00a", "(original, Steam release)"],
 
 		"09818929cceea76ff4c65630e649bbccdb85d92b2e28bb4dfe6526749e6f4fb6": ["th13", "v0.01a", "(original)"],
@@ -77,7 +77,7 @@
 		"3fce325a063439c2e78fd0f95db519f1c83df7c3cf7b6c36e5ca46f7993f97ff": ["th13", "v1.00a", "Aero patch"],
 		"ac0f9d9d0dc92739b52661c5e9f5bf4bab13349bedec1cc87b2330d00f776a2d": ["th13", "v1.00b", "(original)"],
 		"422f40aa176c9aab7d0178ce6033802b4143921a5e0e463ef99259ef6d100a58": ["th13", "v1.00c", "(original)"],
-		"2368f892d2d15ce96e546de33290501bccd03188b875649012d43616314d21c3": ["th13", "v1.00c", "English patch"],
+		"2368f892d2d15ce96e546de33290501bccd03188b875649012d43616314d21c3": ["th13", "v1.00c", "English patch (not recommended)"],
 		"1a6e3eb3b65238a739d7250606e9cb233d16b505c3340a11c4584b583388930d": ["th13", "v1.00c", "(original, Steam release)"],
 
 		"72a7e52d978fb7fcfc0a3a2ced1096305b9161279a029bbf9df2f0b8cb0fe84a": ["th14", "v0.01a",  "(original, Reitaisai trial)"],
@@ -118,7 +118,7 @@
 		"ea8d5c1072f41885ed487a3729202e8c61291c479d5e9ac06b1245f326a4cb2d": ["th06_custom", "v0.13", "(original)"],
 		"d5b62ca04c11fe877d3d02246fa514c11330b22ef0469b443826500db6c4d4b4": ["th06_custom", "v1.00", "(original)"],
 		"5ab1f4e8bf31fc4f8484ab0882a882f0fb399ba758fc98601ae9a6f6e69b5cdf": ["th06_custom", "v1.02h", "(original)"],
-		"27ee5bb9028c86789d66b8689b74e6cbf523dfaf28b28e47766357f662a0b9b0": ["th06_custom", "v1.02h", "English patch"],
+		"27ee5bb9028c86789d66b8689b74e6cbf523dfaf28b28e47766357f662a0b9b0": ["th06_custom", "v1.02h", "English patch (not recommended)"],
 
 		"7bee7ce2580f93c2d436991f2d65d2936f036d0e788d426f3deeb16ff760a9f7": ["th07_custom", "v1.00", "(original)"],
 		"ae36be0c4ff88b5100c65ff63819442e34d60df818da5cc05fdbb61e77ae9fb7": ["th07_custom", "v1.00b", "(original)"],
@@ -130,34 +130,34 @@
 
 		"0e13545781a42b641842410653cf31705f91f9a03104b89d41a8b3a542fad488": ["th09_custom", "v1.00", "(original, v0.02a)"],
 		"13206ce957266aac12cbe3d7b59076c12d17f17acca31fc4a8e642f61f31b7c3": ["th09_custom", "v1.00", "(original)"],
-		"a804e44167ed460598372f1893a87791592e389cea9a54b22d6e6cb0b23915b6": ["th09_custom", "v1.00", "English patch"],
+		"a804e44167ed460598372f1893a87791592e389cea9a54b22d6e6cb0b23915b6": ["th09_custom", "v1.00", "English patch (not recommended)"],
 
 		"c214e44a78d52362326ba6ddb15fdba3c5dd55b71b9bbca9dff7273eb0a7815b": ["th095_custom", "v1.00a", "(original)"],
-		"eb5198697ad93f41b856dbf3f573abc21d7587635efa523046868e782c286c48": ["th095_custom", "v1.00a", "English patch v1.1"],
+		"eb5198697ad93f41b856dbf3f573abc21d7587635efa523046868e782c286c48": ["th095_custom", "v1.00a", "English patch v1.1 (not recommended)"],
 
 		"814373224e8dba9b43886bd0663f44148c314a4cea9af63dce37bf2b7f65590d": ["th10_custom", "v1.00a", "(original)"],
-		"01557b314306c2af5f03cacb13217c1b158c36f4ce61e9d978d036b4c3dda62e": ["th10_custom", "v1.00a", "English patch"],
+		"01557b314306c2af5f03cacb13217c1b158c36f4ce61e9d978d036b4c3dda62e": ["th10_custom", "v1.00a", "English patch (not recommended)"],
 
 		"d56e2126ee3d9ac24c1c0b67e24dff021874384afb7c5e7a4e32348ded5ea1ba": ["alcostg_custom", "v1.00a", "(original)"],
 
 		"cd33e7d9e9d1191cdabdf7e9b63e1de576af60e1b30c1cd4fe9b43920eb9c8de": ["th11_custom", "v1.00a", "(original, v0.02a)"],
 		"ce3ed8afede42074a993eb9d3114420ccb96e8717ae207d3257fb2cd94836126": ["th11_custom", "v1.00a", "(original)"],
-		"405926e10d8bce204dd2b090ae827046b64c373d6d993289dfe40e16c81ad169": ["th11_custom", "v1.00a", "English patch"],
+		"405926e10d8bce204dd2b090ae827046b64c373d6d993289dfe40e16c81ad169": ["th11_custom", "v1.00a", "English patch (not recommended)"],
 		"45730fa1f9463c65f44165a1b8682b628f4b5ee32e60db10f412a0fa4178b62f": ["th11_custom", "v1.00a", "Korean patch"],
 
 		"f3f40228fb8f46d6e6ea74c6643669bd6c486e71e5ec51b6cf2e03640fae83f2": ["th12_custom", "v0.02a", "(original)"],
 		"80d404fa2f616e8043744ade0dd9441dda09f4c6be0bd60bb053ac37d4530da5": ["th12_custom", "v1.00a", "(original)"],
-		"ca124ef6ab5a7d5deb6a5dbf4b486f9b402d7d7d3193f6cb1eefe03c50ada687": ["th12_custom", "v1.00a", "English patch"],
+		"ca124ef6ab5a7d5deb6a5dbf4b486f9b402d7d7d3193f6cb1eefe03c50ada687": ["th12_custom", "v1.00a", "English patch (not recommended)"],
 
 		"84bc3e4a65b5c26bf1f3fbbc44d558dafe0e3ab5de3e20e2f1951859ff200baa": ["th125_custom", "v1.00a", "(original)"],
-		"21889e2e85b98d0f73a2b94f67576480c558ec9edc6a8f9ffbe5eddd031c3d55": ["th125_custom", "v1.00a", "English patch"],
+		"21889e2e85b98d0f73a2b94f67576480c558ec9edc6a8f9ffbe5eddd031c3d55": ["th125_custom", "v1.00a", "English patch (not recommended)"],
 
 		"f3a7fc52dbdf41158eb53ec329de7ef3b6701a8d7a18871a602ea9341a479163": ["th128_custom", "v1.00a", "(original)"],
-		"9a182a73da9095505f86f3d27a86ed8b06f3c713193e28a2625b8e7dcb8b620c": ["th128_custom", "v1.00a", "English patch"],
+		"9a182a73da9095505f86f3d27a86ed8b06f3c713193e28a2625b8e7dcb8b620c": ["th128_custom", "v1.00a", "English patch (not recommended)"],
 
 		"345886950535b5e226783fde6d70087539eb97e1028d87b6c582318c39705e45": ["th13_custom", "v0.01a", "(original)"],
 		"dcc1ae4e2759e34c5784325f279f8201646140bd7ac916ae637c386a5e54e082": ["th13_custom", "v1.00a", "(original)"],
-		"e2a2800d3fdeecc7f2b64ac6457417c4239a83e07e7f017b4f9e205a4c4c050c": ["th13_custom", "v1.00a", "English patch"],
+		"e2a2800d3fdeecc7f2b64ac6457417c4239a83e07e7f017b4f9e205a4c4c050c": ["th13_custom", "v1.00a", "English patch (not recommended)"],
 
 		"331982f40bd21ec8835a71ca4c45d09b15d3e2c41d931a149ab9ced3b72fc2d1": ["th14_custom", "v0.01a",  "(original, Reitaisai trial)"],
 		"026ca8efc4d88b9e3661188dcf0b4a23d7e6517d456cb2c9b02df2b4059850d5": ["th14_custom", "v0.01b",  "(original, web trial)"],
@@ -194,13 +194,13 @@
 		"607744": ["th07", "v1.00",  "(original)"],
 		"626688": ["th07", "v1.00a", "(original)"],
 		"650752": ["th07", "v1.00b", "(original)"],
-		"652288": ["th07", "v1.00b", "English patch"],
+		"652288": ["th07", "v1.00b", "English patch (not recommended)"],
 
 		"825344": ["th08", "v0.03",  "(original)"],
 		"840192": ["th08", "v1.00",  "(original)"],
 		"841216": ["th08", "v1.00a", "(original)"],
 		"840704": ["th08", "v1.00d", "(original)"],
-		"844800": ["th08", "v1.00d", "English patch"],
+		"844800": ["th08", "v1.00d", "English patch (not recommended)"],
 
 		"649728": ["th09", "v1.00",  "(original)"],
 		"685056": ["th09", "v1.50a", "(original)"],
@@ -229,7 +229,7 @@
 		"767488": ["th13", "v0.01a", "(original)"],
 		"789504": ["th13", "v1.00a", "(original)"],
 		"790528": ["th13", "v1.00c", "(original)"],
-		"791041": ["th13", "v1.00c", "English patch"],
+		"791041": ["th13", "v1.00c", "English patch (not recommended)"],
 		"931984": ["th13", "v1.00c", "(original, Steam release)"],
 
 		"878592": ["th14", "v0.01a",  "(original, Reitaisai trial)"],
@@ -261,7 +261,7 @@
 		"747520": ["th17", "v1.00b", "(original)"],
 		"888976": ["th17", "v1.00b", "(original, Steam release)"],
 
-		"102400": ["th06_custom", "v1.02h", "English patch"],
+		"102400": ["th06_custom", "v1.02h", "English patch (not recommended)"],
 		 "98304": ["th08_custom", "v1.00", "Chinese patch"],
 		 "94208": ["alcostg_custom", "v1.00a", "(original)"],
 		 "90112": ["th12_custom", "v1.00a", "(original)"],
