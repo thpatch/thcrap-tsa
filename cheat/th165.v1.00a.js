821ae1735e7a96af7e1ebc6b6c3bd5dc1f868544
rosenrose <crazytempler@gmail.com>
2020-02-06 21:43:05 +0900

    Cheat: Add TH16.5 invincible, Boss timer freeze.

---
+++ cheat/th165.v1.00a.js
@@ -0,0 +1,15 @@
+{
+	"binhacks": {
+		"Main/Invincible": {
+			"addr": "0x4469E0",
+			"code": "C3",
+			"expected": "55"
+		},
+		"Main/Boss timer freeze": {
+			"addr": "0x419AAD",
+			"code": "EB20",
+			"expected": "7605",
+			"ignore": true
+		}
+	}
+}
