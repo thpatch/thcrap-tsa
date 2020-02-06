5642a0918f6a527994357320fa55b9b8029572d7
rosenrose <crazytempler@gmail.com>
2020-02-06 15:25:55 +0900

    Cheat: Add TH09 cheats

--- cheat/th09.v1.50a.js
+++ cheat/th09.v1.50a.js
@@ -4,6 +4,67 @@
 			"addr": "0x417AAF",
 			"code": "EB08",
 			"expected": "D901"
+		},
+		"Main/Infinite Health Points#1(P1)": {
+			"addr": "0x48DAE0",
+			"code": "833DA47E4A0004740A837E08000F84490BF9FF8B86A8000000E97209F9FF"
+		},
+		"Main/Infinite Health Points#1(P2)": {
+			"addr": "0x48DAE0",
+			"code": "833DA47E4A0004740A837E08010F84490BF9FF8B86A8000000E97209F9FF",
+			"ignore": true
+		},
+		"Main/Infinite Health Points#1(both)": {
+			"addr": "0x48DAE0",
+			"code": "833DA47E4A00040F854F0BF9FF8B86A8000000E97809F9FF",
+			"ignore": true
+		},
+		"Main/Infinite Health Points#2": {
+			"addr": "0x41E46A",
+			"code": "2EE970F606"
+		},
+		"Main/Invincible#1(P1)": {
+			"addr": "0x48DB20",
+			"code": "833DA47E4A0004740583FB0B74028B01C3",
+			"ignore": true
+		},
+		"Main/Invincible#1(P2)": {
+			"addr": "0x48DB20",
+			"code": "833DA47E4A0004740583FB0C74028B01C3",
+			"ignore": true
+		},
+		"Main/Invincible#1(both)": {
+			"addr": "0x48DB20",
+			"code": "833DA47E4A000475028B01C3",
+			"ignore": true
+		},
+		"Main/Invincible#2": {
+			"addr": "0x41E8B3",
+			"code": "69F206",
+			"ignore": true
+		},
+		"Main/Max Charge gauage#1(P1)": {
+			"addr": "0x48DB00",
+			"code": "833DA47E4A00047406837E08007405DFE0F6C401C3"
+		},
+		"Main/Max Charge gauage#1(P2)": {
+			"addr": "0x48DB00",
+			"code": "833DA47E4A00047406837E08017405DFE0F6C401C3",
+			"ignore": true
+		},
+		"Main/Max Charge gauage#1(both)": {
+			"addr": "0x48DB00",
+			"code": "833DA47E4A00040F85E9E1F8FFDFE0F6C401E9DDE1F8FF",
+			"ignore": true
+		},
+		"Main/Max Charge gauage#2(P1 or P2)": {
+			"addr": "0x41BCEF",
+			"code": "E80C1E0700"
+		},
+		"Main/Max Charge gauage#2(both)": {
+			"addr": "0x41BCEF",
+			"code": "E90C1E0700",
+			"ignore": true
 		}
 	}
 }
