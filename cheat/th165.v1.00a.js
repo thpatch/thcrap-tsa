e446e931365c5d50eaa1ec6ec057e8c3ae6310f6
rosenrose <crazytempler@gmail.com>
2020-02-08 19:33:41 +0900

    Cheat: TH165 Charge rate is always 100%.

--- cheat/th165.v1.00a.js
+++ cheat/th165.v1.00a.js
@@ -10,6 +10,18 @@
 			"code": "EB20",
 			"expected": "7605",
 			"ignore": true
-		}
+		},
+		"Main/Charge rate is always 100%#1": {
+			"addr": "0x44C5A7",
+			"code": "E9 4E8E0400"
+		},
+		"Main/Charge rate is always 100%#2": {
+			"addr": "0x4953FA",
+			"code": "C746 7C 0000803F"
+		},
+		"Main/Charge rate is always 100%#3": {
+			"addr": "0x495401",
+			"code": "E9 A671FBFF"
+		}		
 	}
 }
