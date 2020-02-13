0c35f8e9a82b709fb7993581491c15766bcd8aa1
rosenrose <42435748+rosenrose@users.noreply.github.com>
2020-02-13 15:20:26 +0900

    Cheat: Th17 - Reduce the code chunks

--- cheat/th17.v1.00b.js
+++ cheat/th17.v1.00b.js
@@ -23,55 +23,23 @@
 			"addr": "0x447B9D",
 			"code": "C0"
 		},
-		"Main/Roaring/Freeze Timer#1-1": {
+		"Main/Roaring/Freeze Timer#1": {
 			"addr": "0x40EE79",
 			"code": "E9 4EB00800"
 		},
-		"Main/Roaring/Freeze Timer#1-2": {
+		"Main/Roaring/Freeze Timer#2": {
 			"addr": "0x499ECC",
-			"code": "A3 08594B00",
+			"code": "A3 08594B00 A1 4CEA4A00 E9 A34FF7FF",
 			"comment": "Save current status."
 		},		
-		"Main/Roaring/Freeze Timer#1-3": {
-			"addr": "0x499ED1",
-			"code": "A1 4CEA4A00"
-		},
-		"Main/Roaring/Freeze Timer#1-4": {
-			"addr": "0x499ED6",
-			"code": "E9 A34FF7FF"
-		},
-		"Main/Roaring/Freeze Timer#2-1": {
+		"Main/Roaring/Freeze Timer#3": {
 			"addr": "0x40EF1D",
 			"code": "E9 B9AF0800 9090909090"
 		},
-		"Main/Roaring/Freeze Timer#2-2": {
+		"Main/Roaring/Freeze Timer#4": {
 			"addr": "0x499EDB",
-			"code": "803D 08594B00 06",
+			"code": "803D 08594B00 06 74 09 803D 08594B00 0E 75 04 66 0FEFC0 8B35 D0774B00 E9 2B50F7FF",
 			"comment": "Check saved status.(0x2: normal, 0x6: Press X, 0xE: Hit by bullet)"
-		},
-		"Main/Roaring/Freeze Timer#2-3": {
-			"addr": "0x499EE2",
-			"code": "74 09"
-		},
-		"Main/Roaring/Freeze Timer#2-4": {
-			"addr": "0x499EE4",
-			"code": "803D 08594B00 0E"
-		},
-		"Main/Roaring/Freeze Timer#2-5": {
-			"addr": "0x499EEB",
-			"code": "75 04"
-		},
-		"Main/Roaring/Freeze Timer#2-6": {
-			"addr": "0x499EED",
-			"code": "66 0FEFC0"
-		},
-		"Main/Roaring/Freeze Timer#2-7": {
-			"addr": "0x499EF1",
-			"code": "8B35 D0774B00"
-		},
-		"Main/Roaring/Freeze Timer#2-8": {
-			"addr": "0x499EF7",
-			"code": "E9 2B50F7FF"
 		},
 		"Main/Roaring/Doesn't decrease on shooting#1": {
 			"addr": "0x411267",
@@ -79,20 +47,8 @@
 		},
 		"Main/Roaring/Doesn't decrease on shooting#2": {
 			"addr": "0x499EBA",
-			"code": "81F9 A45A4B00",
+			"code": "81F9 A45A4B00 74 05 F3 0F1149 08 E9 A073F7FF",
 			"comment": "At roaring mode, ECX is 0x4B5AA4."
-		},
-		"Main/Roaring/Doesn't decrease on shooting#3": {
-			"addr": "0x499EC0",
-			"code": "74 05"
-		},
-		"Main/Roaring/Doesn't decrease on shooting#4": {
-			"addr": "0x499EC2",
-			"code": "F3 0F1149 08"
-		},
-		"Main/Roaring/Doesn't decrease on shooting#5": {
-			"addr": "0x499EC7",
-			"code": "E9 A073F7FF"
 		},
 		"Main/Auto-bomb": {
 			"addr": "0x447C19",
@@ -127,15 +83,7 @@
 		},
 		"Main/Max Point item#2": {
 			"addr": "0x499EFC",
-			"code": "A1 2C5A4B00"
-		},
-		"Main/Max Point item#3": {
-			"addr": "0x499F01",
-			"code": "A3 245A4B00"
-		},
-		"Main/Max Point item#4": {
-			"addr": "0x499F06",
-			"code": "E9 FE64F9FF"
+			"code": "A1 2C5A4B00 A3 245A4B00 E9 FE64F9FF"
 		}
 	}
 }
