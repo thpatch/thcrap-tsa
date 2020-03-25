ccab06584459ad02946abb9628bbba999e1ccc19
32th System <intensonat@gmail.com>
2020-03-25 12:35:40 +0100

    th07: base_tsa stringdefs for stats alignment

--- base_tsa/stringdefs.js
+++ base_tsa/stringdefs.js
@@ -8,6 +8,8 @@
 	"th06_ascii_replay_stage_header": "Stage   LastScore",
 	"th06_ascii_replay_stage": "%-6s %10d",
 	"th06_ascii_replay_stage_empty": "%-6s  ---------",
+	"th07 Stats Character Format": "<l$%s> %6d\t %6d\t %6d\t %6d\t %6d\t %6d",
+	"th07 Stats Character Format +Phantasm": "<l$%s> %6d\t %6d\t %6d\t %6d\t %6d\t %6d\t %6d",
 	"th10 Result Known Spell": "No. <r$%3d$999>  %s <r$%4d/%4d$>",
 	"th10 Result Unknown Spell": "No. <r$%3d$999>  ？？？？？？？？？？？？？？？？？？？？？ <r$%4d/%4d$>",
 	"th15 Result Known Spell": "No.<r$%s%s%s$９９９> %s <r$%4d/%4d$>",
