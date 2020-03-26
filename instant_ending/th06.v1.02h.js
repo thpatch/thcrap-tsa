7c9a841c5ea245031b3e6ec47fdd06a39802db94
32th System <intensonat@gmail.com>
2020-03-26 17:48:57 +0100

    instant_ending: th06: fix address

--- instant_ending/th06.v1.02h.js
+++ instant_ending/th06.v1.02h.js
@@ -8,7 +8,7 @@
 			"code": "0fb605 b8d46900 40 a2 b8d46900 a3 a0bc6900 a3 a4bc6900 9090"
 		},
 		"instant_ending": {
-			"addr": "Rx18d27",
+			"addr": "Rx18f27",
 			"code": "c605 d4d6690006 9090"
 		}
 	}
