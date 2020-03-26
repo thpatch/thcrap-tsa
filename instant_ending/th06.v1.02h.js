698f7c40ba9a086d16c3f418971e361305975f36
32th System <intensonat@gmail.com>
2020-03-26 17:44:07 +0100

    instant_ending: th06

--- instant_ending/th06.v1.02h.js
+++ instant_ending/th06.v1.02h.js
@@ -6,6 +6,10 @@
 		"deaths_to_continues": {
 			"addr": "Rx28dd9",
 			"code": "0fb605 b8d46900 40 a2 b8d46900 a3 a0bc6900 a3 a4bc6900 9090"
+		},
+		"instant_ending": {
+			"addr": "Rx18d27",
+			"code": "c605 d4d6690006 9090"
 		}
 	}
 }
