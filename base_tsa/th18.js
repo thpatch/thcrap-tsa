35311120083a6148e59e92da0c97483c9a8ded63
32th System <intensonat@gmail.com>
2021-03-21 12:47:14 +0100

    th18tr: file breakpoints

--- base_tsa/th18.js
+++ base_tsa/th18.js
@@ -2,5 +2,20 @@
 	"title": "東方虹龍洞　〜 Unconnected Marketeers",
 	"latest": [
 		"v0.01b"
-	]
+	],
+	"breakpoints": {
+		"file_size": {
+			"file_size": "eax",
+			"file_name": "ebx",
+			"cavesize": 5
+		},
+		"file_load": {
+			"file_buffer": "edi",
+			"stack_clear_size": 8,
+			"cavesize": 5
+		},
+		"file_loaded": {
+			"cavesize": 5
+		}
+	}
 }
--- base_tsa/th18.v0.01b.js
+++ base_tsa/th18.v0.01b.js
@@ -1,2 +1,13 @@
 {
+	"breakpoints": {
+		"file_size": {
+			"addr": "Rx211c"
+		},
+		"file_load": {
+			"addr": "Rx2154"
+		},
+		"file_loaded": {
+			"addr": "Rx6588b"
+		}
+	}
 }
