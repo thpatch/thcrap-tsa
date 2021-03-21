b17f8688775efa4b5e4899ff699f06a3df586a76
32th System <intensonat@gmail.com>
2021-03-21 12:56:59 +0100

    th18tr: font block

--- base_tsa/th18.v0.01b.js
+++ base_tsa/th18.v0.01b.js
@@ -9,5 +9,20 @@
 		"file_loaded": {
 			"addr": "Rx6588b"
 		}
+	},
+	"tsa_font_block": {
+		"Rx1476a0",
+		"Rx1476a4",
+		"Rx1476a8",
+		"Rx1476ac",
+		"Rx1476b0",
+		"Rx1476b4",
+		"Rx1476b8",
+		"Rx1476bc",
+		"Rx14769c",
+		"Rx147698",
+		"Rx1476c0",
+		"Rx1476c4",
+		"Rx147698"
 	}
 }
