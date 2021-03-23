d93705a84c967c5ddd834dc6d21e33e70201e425
Egor <egor@opensrc.club>
2021-03-23 12:18:20 +0200

    th18tr v0.02a: spell_id, spell_name

---
+++ base_tsa/th18.v0.02a.js
@@ -0,0 +1,19 @@
+{
+	"breakpoints": {
+		"spell_id": {
+			"addr": "Rx2C7F4"
+		},
+		"spell_id#real": {
+			"addr": "Rx232E4"
+		},
+		"spell_name": {
+			"addr": "Rx234E3"
+		},
+		"spell_id#result": {
+			"addr": "Rx61587"
+		},
+		"spell_name#result": {
+			"addr": "Rx616CD"
+		}
+	}
+}
