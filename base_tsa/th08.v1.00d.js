cc8f8dbf90add9eebc50a7366d9dd9f97af6af10
32th System <intensonat@gmail.com>
2021-04-22 21:35:05 +0200

    th08: fix crash for invalid scorefile checksum
    Co-authored-by: zero318

--- base_tsa/th08.v1.00d.js
+++ base_tsa/th08.v1.00d.js
@@ -1,6 +1,8 @@
 {
 	"codecaves": {
-		"stage_result_align": "56 8d75 f4 6a 00 68 004f4b00 e8[strings_lookup] 89c1 41 8339 00 75 fa 29c1 c1e1 03 b8 c8000000 2bc1 8906 db06 d91e 83c4 08 5e c3"
+		"stage_result_align": "56 8d75 f4 6a 00 68 004f4b00 e8[strings_lookup] 89c1 41 8339 00 75 fa 29c1 c1e1 03 b8 c8000000 2bc1 8906 db06 d91e 83c4 08 5e c3",
+		"TH8ScorefileFixer_VRSMContents": "303130306400 // 0000 // 00D40C00 // B96168A2",
+		"TH8ScorefileFixer_FixVRSM": "85C0 // 74 0C // 40 // C3 // CCCCCCCCCCCCCCCCCCCC // 0F2805 <codecave:TH8ScorefileFixer_VRSMContents> // 0F1101 // 0F1745 0C // 89C8 // C3"
 	},
 	"binhacks": {
 		"antitamper_remove_check": {
@@ -83,6 +85,11 @@
 		"stage_result_align_call": {
 			"addr": "Rx382ac",
 			"code": "e8[codecave:stage_result_align] 8d45 f4 90 7d12 68 1c4f4b00 50 68 20ce4c00 e8 69a7fcff eb 25 833d ccd26401 07 7c 0c d945 f8 d825 d4424b00 d95d f8 68 104f4b00"
+		},
+		"TH8ScorefileFixer": {
+			"addr": "0x44869D",
+			"code": "83C4 08 // 8B4D 08 // E8 [codecave:TH8ScorefileFixer_FixVRSM] // 8945 FC // 2B45 F0 // 2945 F4",
+			"expected": "59 // 59 // 40 // 8945 FC // 8B45 FC // 2B45 F0 // 8B4D F4 // 2BC8 // 894D F4"
 		}
 	},
 	"breakpoints": {
