c459649d1a081a9e959cada4bcae2ecd95ec8028
brliron <brliron@hotmail.fr>
2021-06-13 20:08:14 +0200

    base_tsa: remove -.- from the th06 end binhacks
    They're nice and descriptive, but they make the game crash -.-

--- base_tsa/th06.v1.02h.js
+++ base_tsa/th06.v1.02h.js
@@ -137,7 +137,7 @@
 		"unpatch_ending_halfskip": {
 			"addr": "Rx10521",
 			"title": "Unpatch the static English patch's rendering skip of the second-half ending text sprite",
-			"code": "-.-7402EB21C745FC01000000C745CC00000000B90900000033C08D7DD0-.-"
+			"code": "7402EB21C745FC01000000C745CC00000000B90900000033C08D7DD0"
 		},
 		"remove_score_cap": {
 			"addr": ["0x41b8b0", "0x41b8bc"]
