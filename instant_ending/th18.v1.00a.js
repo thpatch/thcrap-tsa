f5e4901ed2eb76d9ed6876b9baa38ae93964366a
32th-System <intensonat@gmail.com>
2021-09-13 11:48:28 +0200

    instant_ending: th18

---
+++ instant_ending/th18.v1.00a.js
@@ -0,0 +1,14 @@
+{
+	"binhacks": {
+		"instant_ending": {
+			"addr": "Rx448c1"
+		},
+		"zero_lives": {
+			"addr": "Rx42930"
+		},
+		"deaths_to_continues": {
+			"addr": "Rx5d19b",
+			"code": "a104cd4c0083c001a304cd4c00"
+		}
+	}
+}
