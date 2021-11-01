f52b2919e1d1a3a45f7e75c12a594388362b6cd6
32th-System <intensonat@gmail.com>
2021-11-02 00:57:27 +0100

    th175: file breakpoints

---
+++ base_tasofro/th175.v1.01.js
@@ -0,0 +1,14 @@
+{
+	"breakpoints": {		
+		"th175_open_file": {
+			"addr": "Rx19177"
+		},
+		"th175_replaceReadFile": {
+			"file_handle": "esi + 4",
+			"addr": "Rx19ccc"
+		},
+		"th175_close_file": {
+			"addr": "Rx1b760"
+		}
+	}
+}
