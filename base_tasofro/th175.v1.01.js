f0574dabbb5a43003488ed87d9f5ade011293d30
brliron <brliron@hotmail.fr>
2021-12-22 23:46:33 +0100

    base_tasofro: th175: fix a crash when the game exits
    Fixes #163

--- base_tasofro/th175.v1.01.js
+++ base_tasofro/th175.v1.01.js
@@ -8,6 +8,14 @@
 		},
 		remove_fullscreen: {
 			addr: "Rx4590"
+		},
+		remove_cleanup: {
+			code: "9090 9090 90 9090",
+			addr: "Rx14219",
+			title: "Remove a bugged cleanup function"
+			// This cleanup function tend to crash a lot when we close the game,
+			// and, well, it's only called when the game exits, so we don't care
+			// if it doesn't free some resources before the OS frees the entire process.
 		}
 	},
 	"breakpoints": {		
@@ -22,6 +30,14 @@
 		},
 		"th175_close_file": {
 			"addr": "Rx1b760"
+		},
+		/*
+		 * Not used (yet)
+		detour_plugin: {
+			cavesize: 6,
+			plugin: "eax",
+			addr: "Rx141dc"
 		}
+		*/
 	}
 }
