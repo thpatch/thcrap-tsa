8e054affc76108ffdab8060579ed35b4f7ed3d89
32th-System <intensonat@gmail.com>
2022-06-25 16:21:29 +0200

    th10: force original font for font ID 15
    Font ID 15 is used by the hint system. I wasn't able to figure out an
    absolutely perfect right and center alignment hack that would work with
    any font, so I decided to not do any alignment hacking if the text
    output functions are being called by the hint system, and forced the
    hint system to use the original font no matter what. A lot of hint files
    actually rely on the properties of MS Gothic and the ones that I
    recieved from GitHub issues try to create lines that span across the
    entire STG frame with dashes, and the dashes in Touhou Biolinum are
    simply too small.
    
    fixes #47

--- base_tsa/th10.v1.00a.js
+++ base_tsa/th10.v1.00a.js
@@ -7,6 +7,18 @@
 		"th10_spell_align": {
 			"access": "re",
 			"code": "56 57 8b7424 0c 8b7e 10 81bf 9c000000 c2944100 75 08 b8 01000000 5f 5e c3 ff76 0c ff77 18 e8[GetTextExtentForFontID] 8946 1c eb e8"
+		},
+		"th10_font_15": {
+			"access": "rw",
+			"size": 4
+		},
+		"th10_load_font_15": {
+			"access": "re",
+			"code": "68<option:gdi32_dll_name_w> e8[th_GetModuleHandleW] 68<option:CreateFontW_name> 50 e8[th_GetProcAddress] 68<option:MS_Gothic_name_w> 6a 11 6a 04 6a 00 6a 00 68 80000000 6a 00 6a 00 6a 00 68 90010000 6a 00 6a 00 6a 00 6a 20 ffd0 a3<codecave:th10_font_15> 5e c3"
+		},
+		"th10_use_font_15": {
+			"access": "re",
+			"code": "83fb 0f 75 10 833d<codecave:th10_font_15> 00 74 07 8b15<codecave:th10_font_15> c3 8b15 a0184900 c3"
 		}
 	},
 	"binhacks": {
@@ -67,7 +79,16 @@
 			"title": "Fix the crash after the end-of-stage dialogue in Practice replays, #2 (code cave)",
 			"addr": "0x465b00",
 			"code": "f605a04c470010 7403 33c0 c3 a110784700 83785c00 7506 b801000000 c3 a16c4c4700 c3"
+		},
+		"load_font_15": {
+			"addr": "Rx37d0b",
+			"code": "e9[codecave:th10_load_font_15]"
+		},
+		"use_font_15": {
+			"addr": "Rx37dc0",
+			"code": "e8[codecave:th10_use_font_15] 90"
 		}
+			
 	},
 	"breakpoints": {
 		"file_size": {
