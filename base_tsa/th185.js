754e3c35e3e78c78ad6d3d4136ba67d6b392b5ec
32th-System <intensonat@gmail.com>
2022-08-14 10:12:12 +0200

    th185: file breakpoints

---
+++ base_tsa/th185.js
@@ -0,0 +1,22 @@
+{
+	"title": "バレットフィリア達の闇市場　〜 100th Black Market",
+	"latest": [
+		"v1.00a"
+	],
+	"breakpoints": {
+		"file_size": {
+			"file_size": "eax",
+			"file_name": "ebx",
+			"cavesize": 5
+		},
+		"file_load": {
+			"file_buffer": "edi",
+			"stack_clear_size": 8,
+			"cavesize": 5
+		},
+		"file_loaded": {
+			"cavesize": 5
+		}
+	}
+	}
+}
---
+++ base_tsa/th185.v1.00a.js
@@ -0,0 +1,13 @@
+{
+	"breakpoints": {
+		"file_size": {
+			"addr": "Rx213c"
+		},
+		"file_load": {
+			"addr": "Rx2174"
+		},
+		"file_loaded": {
+			"addr": "Rx7144e"
+		}
+	}
+}
