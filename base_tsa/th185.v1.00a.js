c4450fb863d920b1a0701b1611dfc3dd915e57fb
32th-System <intensonat@gmail.com>
2022-08-14 10:30:10 +0200

    th185: font block

--- base_tsa/th185.v1.00a.js
+++ base_tsa/th185.v1.00a.js
@@ -9,5 +9,21 @@
 		"file_loaded": {
 			"addr": "Rx7144e"
 		}
-	}
+	},
+	"tsa_font_block": [
+		"Rx179c28",
+		"Rx179c2c",
+		"Rx179c30",
+		"Rx179c34",
+		"Rx179c38",
+		"Rx179c3c",
+		"Rx179c40",
+		"Rx179c44",
+		"Rx179c24",
+		"Rx179c20",
+		"Rx179c48",
+		"Rx179c4c",
+		"Rx179c50",
+		"Rx179c20"
+	]
 }
