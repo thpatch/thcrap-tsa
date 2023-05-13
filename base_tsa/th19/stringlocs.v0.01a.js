8e6b5279927024644ff3a46b39ab8f10209a1b9b
Liu Wenyuan <15816141883@163.com>
2023-05-13 18:12:33 +0800

    th19 v0.01a: identify some hardcoded strings

---
+++ base_tsa/th19/stringlocs.v0.01a.js
@@ -0,0 +1,18 @@
+{
+	"Rxbbb38": "th19_scorefile_fn",
+	"Rxbbb24": "th19_scorefile_backup_fn",
+
+    "Rxbbba4": "th18 card unobtained",
+    "Rxbbb70": "th18 card unavailable",
+    "Rxbbb84": "th185 equippable slot",
+    "Rxbbb94": "th185 unequip",
+
+    //"Rxbbb58": "th19 nothing",
+
+    "Rxbd590": "Music Room Unknown Title",
+    "Rxbd44c": "th10 Music Room spoiler 1",
+    "Rxbd488": "th10 Music Room spoiler 2",
+    "Rxbd4c0": "th10 Music Room spoiler 3",
+    "Rxbd4ec": "th10 Music Room spoiler 4",
+    "Rxbd528": "th10 Music Room spoiler 5"
+}
