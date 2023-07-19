104a124590757b4ad9ba272b375b492d85f7e8c6
32th System <intensonat@gmail.com>
2023-07-19 10:12:57 +0200

    th19: remove duplicate from dictdefs.js

--- base_tsa/th19/dictdefs.js
+++ base_tsa/th19/dictdefs.js
@@ -1,10 +1,9 @@
 {
   "サーバーに接続成功しました": "th19_net_connected",
   "プレイヤーの名前を変更します。": "th19_net_playername_1",
-  "英数文字と一部の記号のみ使えます。": "th19_net_playername_2",
+  "英数文字と一部の記号のみ使えます。": "th19_net_alphanumeric",
   "ルームの名前を変更します。": "th19_net_roomrename_1",
-  "英数文字と一部の記号のみ使えます。": "th19_net_roomrename_2",
-  "一緒に遊びたい人にこのルーム名を伝えてください。": "th19_net_roomrename_3",
+  "一緒に遊びたい人にこのルーム名を伝えてください。": "th19_net_roomrename_2",
   "ルームに参加しようとしてます。": "th19_net_room_joining",
   "ルームに参加しました。": "th19_net_room_joined",
   "対戦相手を待っています。": "th19_net_waiting_for_opponent",
