e67a16f47933df233955d26e9203120cd35b191c
Egor <egor@opensrc.club>
2023-08-13 13:46:06 +0300

    th19 v1.00a: bypass SteamDRM

---
+++ base_tsa/th19.v1.00a.js
@@ -0,0 +1,33 @@
+	"init_stages": [
+		{
+			"binhacks": {
+				"steamstub_crack": {
+					"title": "Crack SteamStub by disabling its integrity check",
+					"addr": "Rx231413",
+					"code": "eb",
+					"expected": "74"
+				}
+			},
+			"breakpoints": {
+				"init_next_stage#1": {
+					"addr": "Rx232036",
+					"module": "eax",
+					"cavesize": 6
+				},
+				"init_next_stage#game": {
+					"addr": "Rx230333",
+					"cavesize": 5
+				}
+			}
+		},
+		{
+			"The addresses here are relative to": "SteamDRMP.dll",
+			"binhacks": {
+				"steamdrm_crack": {
+					"title": "Crack SteamDRM: Remove all communication with the Steam client",
+					"addr": "Rx3e70",
+					"code": "90909090909090909090 b030 88450b e9"
+				}
+			}
+		}
+	]
