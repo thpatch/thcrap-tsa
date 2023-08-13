073b799e387086e0d7aea87baa73ef8517e11208
32th-System <intensonat@gmail.com>
2023-08-13 19:00:17 +0200

    th19 v1.00a: game rules stringlocs

---
+++ base_tsa/th19/stringlocs.v1.00a.js
@@ -0,0 +1,41 @@
+{
+	"Rx178F08": "th19_rules_barrier_manual",    // "手動回復のみ (Manual Only)"
+	"Rx178F24": "th19_rules_barrier_short",    // "時間長め (Long Time)"
+	"Rx178F3C": "th19_rules_barrier_long",    // "時間早め (Short Time)"
+	
+	"Rx178EB8": "th19_rules_cards_all",    // "全てのカードから選択 (All Card)"
+	"Rx178E84": "th19_rules_cards_random",    // "ランダム (Random)"
+	"Rx178DE8": "th19_rules_cards_random",   // "ランダム (Random)"
+	"Rx178E74": "th19_rules_cards_none",    // "無し (no use)"
+	"Rx178E98": "th19_rules_cards_choose",    // "自分のカードのみ (Self Card)"
+	
+	"Rx178E50": "th19_rules_matches_1",    // "１本勝負"
+	"Rx178DE0": "th19_rules_matches_1",   // "1本勝負"
+	"Rx178E5C": "th19_rules_matches_2",    // "２本先取"
+	"Rx178E68": "th19_rules_matches_3",    // "３本先取"
+	"Rx178E14": "th19_0_min",    // "無し (None)"
+	"Rx178E20": "th19_3_min",    // "3 min"
+	"Rx178E28": "th19_5_min",    // "5 min"
+	"Rx178DD8": "th19_5_min",    // "5 min"
+	"Rx178E30": "th19_7_min",    // "7 min"
+	"Rx178E38": "th19_10_min",    // "10 min"
+	"Rx178E40": "th19_15_min",    // "15 min"
+	"Rx178E48": "th19_20_min",    // "20 min"
+	
+	
+	"Rx178C84": "th19_rules_title",   // "対戦ルール＆ハンディキャップ設定"
+	"Rx178CF0": "th19_rules_time",   // "制限時間 (Time Limit)"
+	"Rx178D08": "th19_rules_rounds",   // "ラウンド数 (Round)"
+	"Rx178D1C": "th19_rules_cards",   // "アビリティカード (Ability Card)"
+	"Rx178D3C": "th19_rules_life_1p",   // "1Pライフ(1P Life)"
+	"Rx178D50": "th19_rules_life_2p",   // "2Pライフ(2P Life)"
+	"Rx178D64": "th19_rules_barrier_1p",   // "1Pバリア回復時間(Barrier)"
+	"Rx178D80": "th19_rules_barrier_2p",   // "2Pバリア回復時間(Barrier)"
+	"Rx178D9C": "th19_rules_default",   // "初期設定に戻す (Default Setting)"
+	"Rx178DC0": "th19_rules_quit",   // "設定を終了する (Quit)"
+	
+	//"Rx178E04": "th19_rules_",   // "普通"
+	//"Rx178E0C": "th19_rules_",   // "普通"
+	
+	"Rx17677C": "th19_ability_random", // "アビリティカード：ランダム装備"
+}
