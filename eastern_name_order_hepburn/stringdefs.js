316fde469def5ed3d6a2eb3d8aa43a0f5988a37e
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-09-03 10:58:29 +0200

    Create stringdefs.js

---
+++ eastern_name_order_hepburn/stringdefs.js
@@ -0,0 +1,8 @@
+{
+  "th08 Stats Reimu": "Hakurei Reimu",
+    "th08 Stats Yukari": "Yakumo Yukari",
+    "th08 Stats Marisa": "Kirisame Marisa",
+    "th08 Stats Sakuya": "Izayoi Sakuya",
+    "th08 Stats Youmu": "Konpaku Youmu",
+    "th08 Stats Yuyuko": "Saigyouji Yuyuko"
+}
