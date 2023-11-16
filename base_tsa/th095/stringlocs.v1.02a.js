9c7dba1a8b9acf08fa02af1958ec6c6659c63d83
Eridanus91 <bulbatesting@gmail.com>
2023-11-16 22:01:26 +0100

    Update stringlocs.v1.02a.js
    this game has surprisingly many strings to translate

--- base_tsa/th095/stringlocs.v1.02a.js
+++ base_tsa/th095/stringlocs.v1.02a.js
@@ -1,5 +1,26 @@
 {
+	"Rx956B8": "th095_scene_photo_count",
+	"Rx956C8": "th095_scene_score",
+	"Rx956D8": "th095_scene_hiscore",
 	"Rx96450": "th095_replay_format",
+	"Rx96488": "th095_replay_name_regist",
+	"Rx964A0": "th095_ascii_replay_save",
+	"Rx964D8": "th095_ascii_replay_save_empty",
+	"Rx96514": "th095_select_replay_number",
+	"Rx9652C": "th095_bestshot_overwrite",
+	"Rx96548": "th095_results_slow_rate",
+	"Rx96564": "th095_results_total_score",
+	"Rx96578": "th095_results_high_score",
+	"Rx96578": "th095_results_score",
+	"Rx965A0": "th095_results_shot",
+	"Rx965B4": "th095_results_best_shot",
 	"Rx97884": "th095_replay_user_find",
-	"Rx96864": "th095_scorefile_fn"
+	"Rx978CC": "th095_ascii_user_replay",
+	"Rx97904": "th095_ascii_user_replay_empty",
+	"Rx97938": "th095_ascii_replay",
+	"Rx97970": "th095_ascii_replay_empty",
+	"Rx96864": "th095_scorefile_fn",
+	"Rx979B0": "th095_menu_slow_rate",
+	"Rx979C4": "th095_menu_high_score",
+	"Rx979D4": "th095_menu_scene_success_amount"
 }
