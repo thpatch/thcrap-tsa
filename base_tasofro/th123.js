b7d67d285c1b2f76cd9f1c805a6b1c6340f6b32c
brliron <brliron@hotmail.fr>
2024-04-02 02:10:49 +0200

    Port 3cc5000 to th123

--- base_tasofro/th123.js
+++ base_tasofro/th123.js
@@ -4,10 +4,6 @@
 			"code": "8d041f 50 52 50 e8[tasofro_CharNextA] 5a 59 2bc1 b104 50 2ac8 8b041f 0fc8 84c9 7407 c1e808 fec9 ebf5 59 90909090",
 			"title": "Move a character to eax, without assuming a 2-bytes encoding."
 		},
-		"strings_lookup": {
-			"code": "8bd4 57 52 50 e8[strings_lookup] 59 8bd8 90",
-			"title": "Calls strings_lookup to give thcrap a chance to replace the game's strings."
-		}
 	},
 	"title": "東方非想天則 ~ 超弩級ギニョルの謎を追え",
 	"url_update": "http://tasofro.net/touhou123/download.html",
@@ -52,6 +48,10 @@
 			"string": "edx",
 			"delim": "byte ptr [[ebp+0x0c]]",
 			"cavesize": 19
-		}
+		},
+		strings_lookup: {
+			str: "eax",
+			cavesize: 5
+		},
 	}
 }
