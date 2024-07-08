1ad0cc150f9710d74ef4b40affe7f33aaa0a8bb4
32th-System <intensonat@gmail.com>
2024-07-08 13:47:12 +0200

    th19 v1.10c: file breakpoints

---
+++ base_tsa/th19.v1.10c.js
@@ -0,0 +1,21 @@
+{
+	"codecaves": {
+		"th19_file_load": {
+			"access": "re",
+			"code": "8b4c24048b5424086a00e8[RxD6760]83c404c20800"
+		}
+	},
+	"breakpoints": {
+		"file_size": {
+			"addr": "RxD6804"
+		},
+		"file_load": {
+			"addr": "RxD6842"
+		},
+		"file_loaded": {
+			"addr": "Rx15E800"
+		},
+	},
+	"file_load_func": "<codecave:th19_file_load>",
+	"free_func": "<Rx7DF88>"
+}
