095dc877c70b3820744465367ceb6c4a63ab0509
MasterYova <137089094+MasterYova@users.noreply.github.com>
2025-05-18 23:21:45 +0200

    Adding images for Touhou 10.5 Hisouten and 12.3 Hisoutensoku into script_latin and eastern_name_order_hepburn , and moving the image for Orin in Touhou 12.5 (#103)

--- eastern_name_order_hepburn/stringdefs.js
+++ eastern_name_order_hepburn/stringdefs.js
@@ -1,8 +1,10 @@
 {
-  "th08 Stats Reimu": "Hakurei Reimu",
+    "th08 Stats Reimu": "Hakurei Reimu",
     "th08 Stats Yukari": "Yakumo Yukari",
     "th08 Stats Marisa": "Kirisame Marisa",
     "th08 Stats Sakuya": "Izayoi Sakuya",
     "th08 Stats Youmu": "Konpaku Youmu",
     "th08 Stats Yuyuko": "Saigyouji Yuyuko"
+    "th20 charname Reimu Hakurei full": "Hakurei Reimu",
+    "th20 charname Marisa Kirisame full": "Kirisame Marisa"
 }
