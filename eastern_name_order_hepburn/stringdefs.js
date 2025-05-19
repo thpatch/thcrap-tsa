a61a6c121b9b30c1b592920a9733afa4be22313a
MasterYova <137089094+MasterYova@users.noreply.github.com>
2025-05-19 08:56:19 +0200

    URGENT! Fixing a syntax error in stringdefs.js file I forgot to fix while adding new entries for th20 to it (#104)

--- eastern_name_order_hepburn/stringdefs.js
+++ eastern_name_order_hepburn/stringdefs.js
@@ -4,7 +4,7 @@
     "th08 Stats Marisa": "Kirisame Marisa",
     "th08 Stats Sakuya": "Izayoi Sakuya",
     "th08 Stats Youmu": "Konpaku Youmu",
-    "th08 Stats Yuyuko": "Saigyouji Yuyuko"
+    "th08 Stats Yuyuko": "Saigyouji Yuyuko",
     "th20 charname Reimu Hakurei full": "Hakurei Reimu",
     "th20 charname Marisa Kirisame full": "Kirisame Marisa"
 }
