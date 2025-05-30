3c45caf353a54080398cf7e167bcfdce80cef0c3
Liu Wenyuan <15816141883@163.com>
2025-05-30 23:51:27 +0800

    th20 v0.01a: Support translating resolution dialog

---
+++ base_tsa/th20/dialog_203.js
@@ -0,0 +1,18 @@
+{
+	"title": "th20 JP",
+	"items": [
+		"th14 Dialog Prompt",
+		"th18 Dialog Borderless DOT",
+		"th18 Dialog Borderless",
+		"th18 Dialog Fullscreen 1280",
+		"th18 Dialog Fullscreen 960",
+		"th18 Dialog Fullscreen 640",
+		"th18 Dialog Window 2560",
+		"th18 Dialog Window 1920",
+		"th18 Dialog Window 1280",
+		"th18 Dialog Window 960",
+		"th18 Dialog Window 640",
+		"th16 Dialog Always ask",
+		"th11 Dialog Game Start"
+	]
+}
