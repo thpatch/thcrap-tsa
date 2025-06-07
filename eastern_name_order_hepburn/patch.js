2c67462e41e2fb098d80c4f8f388cfca5e7b6a43
MasterYova <137089094+MasterYova@users.noreply.github.com>
2025-06-07 17:51:26 +0200

    Update patch.js

--- eastern_name_order_hepburn/patch.js
+++ eastern_name_order_hepburn/patch.js
@@ -1,10 +1,10 @@
 {
-  "dependencies": [
-    "base_tsa"
-  ],
-  "id": "eastern_name_order_hepburn",
-  "servers": [
-    "https://mirrors.thpatch.net/nmlgc/eastern_name_order_hepburn/"
-  ],
-  "title": "Character names in eastern order and hepburn spelling."
+	"dependencies": [
+		"base_tsa"
+	],
+	"id": "eastern_name_order_hepburn",
+	"servers": [
+		"https://mirrors.thpatch.net/nmlgc/eastern_name_order_hepburn/"
+	],
+	"title": "Character names in eastern order and hepburn spelling."
 }
